af6aae0fa34e58726fe4df6c8e4ef85fb9fc3aea
Jason van Zyl <jvanzyl@apache.org>
2004-01-13 22:54:37 +0000

    o http://jira.codehaus.org/secure/ViewIssue.jspa?key=XSTR-17
    git-svn-id: http://svn.codehaus.org/xstream/trunk@61 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/FileConverter.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.xstream.converters.extended;
+
+import com.thoughtworks.xstream.converters.basic.AbstractBasicConverter;
+
+import java.io.File;
+
+/**
+ * This converter will take care of storing and retrieving File with either
+ * an absolute path OR a relative path depending on how they were created.
+ */
+public class FileConverter extends AbstractBasicConverter {
+
+    public boolean canConvert(Class type) {
+        return type.equals(File.class);
+    }
+
+    protected Object fromString(String str) {
+        return new File(str);
+    }
+
+    protected String toString(Object obj) {
+        return ((File)obj).getPath();
+    }
+
+}
--- xstream/src/test/com/thoughtworks/acceptance/ExtendedTypesTest.java
+++ xstream/src/test/com/thoughtworks/acceptance/ExtendedTypesTest.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.acceptance;
 
 import com.thoughtworks.xstream.converters.extended.ColorConverter;
+import com.thoughtworks.xstream.converters.extended.FileConverter;
 import com.thoughtworks.xstream.converters.extended.SqlTimestampConverter;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.TimeZone;
 
@@ -37,5 +40,34 @@ public class ExtendedTypesTest extends AbstractAcceptanceTest {
                 "<sql-timestamp>1970-01-01 00:00:01.234</sql-timestamp>");                
     }
 
+    public void testFile() throws IOException {
+        xstream.alias("file", File.class);
+        xstream.registerConverter(new FileConverter());
 
+        // using temp file to avoid os specific or directory layout issues
+        File absFile = File.createTempFile("bleh", ".tmp");
+        absFile.deleteOnExit();
+        assertTrue(absFile.isAbsolute());
+        String expectedXml = "<file>" + absFile.getPath() + "</file>";
+        assertFilesBothWay(absFile, expectedXml);
+
+        // test a relative file now
+        File relFile = new File("bloh.tmp");
+        relFile.deleteOnExit();
+        assertFalse(relFile.isAbsolute());
+        expectedXml = "<file>" + relFile.getPath() + "</file>";
+        assertFilesBothWay(relFile, expectedXml);
+    }
+
+    private void assertFilesBothWay(File f, String expectedXml) {
+        String resultXml = xstream.toXML(f);
+        assertEquals(expectedXml, resultXml);
+        Object resultObj = xstream.fromXML(resultXml);
+        assertEquals(File.class, resultObj.getClass());
+        assertEquals(f, resultObj);
+        // now comes the part that fails without a specific converter
+        // in the case of a relative file, this will work, because we run the comparison test from the same working directory
+        assertEquals(f.getAbsolutePath(), ((File)resultObj).getAbsolutePath());
+        assertEquals(f.isAbsolute(), ((File)resultObj).isAbsolute()); // needed because File's equals method only compares the path attribute, at least in the win32 implementation
+    }
 }
