a8d75ab8e19db1d22775530561e42a2f509ff97f
Joe Walnes <joe@truemesh.com>
2004-03-06 10:57:14 +0000

    Added test submitted by Paul Hammant
    git-svn-id: http://svn.codehaus.org/xstream/trunk@78 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/test/com/thoughtworks/acceptance/AliasTest.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.acceptance;
+
+import com.thoughtworks.someobjects.X;
+import com.thoughtworks.xstream.alias.CannotResolveClassException;
+
+/**
+ * @author Paul Hammant
+ */
+public class AliasTest extends AbstractAcceptanceTest {
+
+   public void testBarfsIfAliasDoesNotExist() {
+
+       String xml = "" +
+               "<X-array>\n" +
+               "  <X>\n" +
+               "    <anInt>0</anInt>\n" +
+               "  </X>\n" +
+               "</X-array>";
+
+       // now change the alias
+       xstream.alias("Xxxxxxxx", X.class);
+       try {
+           xstream.fromXML(xml);
+           fail("Should have barfed");
+       } catch (CannotResolveClassException expectedException) {
+           // expected
+       }
+   }
+
+}
