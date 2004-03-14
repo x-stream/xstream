b6c1329285534ee69017c9a5aaea376ceb6cfa56
Joe Walnes <joe@truemesh.com>
2004-03-14 21:41:45 +0000

    Pushed default aliases and converters into DefaultConverterLookup
    git-svn-id: http://svn.codehaus.org/xstream/trunk@141 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/XStream.java
+++ xstream/src/java/com/thoughtworks/xstream/XStream.java
@@ -2,15 +2,9 @@ package com.thoughtworks.xstream;
 
 import com.thoughtworks.xstream.alias.ClassMapper;
 import com.thoughtworks.xstream.converters.Converter;
-import com.thoughtworks.xstream.converters.basic.*;
-import com.thoughtworks.xstream.converters.collections.*;
-import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
 import com.thoughtworks.xstream.converters.reflection.ReflectionProvider;
 import com.thoughtworks.xstream.converters.reflection.Sun14ReflectionProvider;
-import com.thoughtworks.xstream.core.DefaultClassMapper;
-import com.thoughtworks.xstream.core.DefaultConverterLookup;
-import com.thoughtworks.xstream.core.MarshallingContextAdaptor;
-import com.thoughtworks.xstream.core.UnmarshallingContextAdaptor;
+import com.thoughtworks.xstream.core.*;
 import com.thoughtworks.xstream.io.HierarchicalStreamDriver;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
@@ -21,15 +15,12 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.*;
 
 public class XStream {
 
-    protected DefaultConverterLookup converterLookup = new DefaultConverterLookup();
-    protected HierarchicalStreamDriver hierarchicalStreamDriver;
-    protected ClassMapper classMapper;
-    protected ReflectionProvider reflectionProvider;
-    protected String classAttributeIdentifier;
+    private HierarchicalStreamDriver hierarchicalStreamDriver;
+    private ClassMapper classMapper;
+    private DefaultConverterLookup converterLookup;
 
     public XStream() {
         this(new Sun14ReflectionProvider(), new DefaultClassMapper(), new DomDriver());
@@ -47,75 +38,16 @@ public class XStream {
         this(reflectionProvider, new DefaultClassMapper(), hierarchicalStreamDriver);
     }
 
-    public XStream(ReflectionProvider objectFactory, ClassMapper classMapper, HierarchicalStreamDriver driver) {
-        this(objectFactory, classMapper, driver, "class");
+    public XStream(ReflectionProvider reflectionProvider, ClassMapper classMapper, HierarchicalStreamDriver driver) {
+        this(reflectionProvider, classMapper, driver, "class");
     }
 
-    public XStream(ReflectionProvider objectFactory, ClassMapper classMapper, HierarchicalStreamDriver driver,String classAttributeIdentifier) {
+    public XStream(ReflectionProvider reflectionProvider, ClassMapper classMapper, HierarchicalStreamDriver driver,String classAttributeIdentifier) {
         this.classMapper = classMapper;
-        this.reflectionProvider = objectFactory;
         this.hierarchicalStreamDriver = driver;
-        this.classAttributeIdentifier = classAttributeIdentifier;
 
-        alias("int", Integer.class);
-        alias("float", Float.class);
-        alias("double", Double.class);
-        alias("long", Long.class);
-        alias("short", Short.class);
-        alias("char", Character.class);
-        alias("byte", Byte.class);
-        alias("boolean", Boolean.class);
-        alias("number", Number.class);
-        alias("object", Object.class);
-
-        alias("string-buffer", StringBuffer.class);
-        alias("string", String.class);
-        alias("java-class", Class.class);
-        alias("date", Date.class);
-        alias("bit-set", BitSet.class);
-
-        alias("map", Map.class, HashMap.class);
-        alias("properties", Properties.class);
-        alias("list", List.class, ArrayList.class);
-        alias("set", Set.class, HashSet.class);
-
-        alias("linked-list", LinkedList.class);
-        alias("vector", Vector.class);
-        alias("tree-map", TreeMap.class);
-        alias("tree-set", TreeSet.class);
-        alias("hashtable", Hashtable.class);
-
-        registerConverter(new ReflectionConverter(classMapper,classAttributeIdentifier, objectFactory));
-
-        registerConverter(new IntConverter());
-        registerConverter(new FloatConverter());
-        registerConverter(new DoubleConverter());
-        registerConverter(new LongConverter());
-        registerConverter(new ShortConverter());
-        registerConverter(new CharConverter());
-        registerConverter(new BooleanConverter());
-        registerConverter(new ByteConverter());
-
-        registerConverter(new StringConverter());
-        registerConverter(new StringBufferConverter());
-        registerConverter(new DateConverter());
-        registerConverter(new JavaClassConverter());
-        registerConverter(new BitSetConverter());
-
-        registerConverter(new ArrayConverter(classMapper,classAttributeIdentifier));
-        registerConverter(new CharArrayConverter());
-        registerConverter(new CollectionConverter(classMapper,classAttributeIdentifier));
-        registerConverter(new MapConverter(classMapper,classAttributeIdentifier));
-        registerConverter(new PropertiesConverter());
-
-    }
-
-    public void alias(String elementName, Class type, Class defaultImplementation) {
-        classMapper.alias(elementName, type, defaultImplementation);
-    }
-
-    public void alias(String elementName, Class type) {
-        alias(elementName, type, type);
+        converterLookup = new DefaultConverterLookup(reflectionProvider, classMapper, classAttributeIdentifier);
+        converterLookup.setupDefaults();
     }
 
     public String toXML(Object obj) {
@@ -150,8 +82,16 @@ public class XStream {
     public Object unmarshal(HierarchicalStreamReader reader, Object root) {
         UnmarshallingContextAdaptor context = new UnmarshallingContextAdaptor(
                 root, reader, converterLookup,
-                classMapper, classAttributeIdentifier);
+                classMapper, converterLookup.getClassAttributeIdentifier());
         return context.start();
+    }
+
+    public void alias(String elementName, Class type, Class defaultImplementation) {
+        converterLookup.alias(elementName, type, defaultImplementation);
+    }
+
+    public void alias(String elementName, Class type) {
+        converterLookup.alias(elementName, type, type);
     }
 
     public void registerConverter(Converter converter) {
--- xstream/src/java/com/thoughtworks/xstream/core/DefaultConverterLookup.java
+++ xstream/src/java/com/thoughtworks/xstream/core/DefaultConverterLookup.java
@@ -3,17 +3,30 @@ package com.thoughtworks.xstream.core;
 import com.thoughtworks.xstream.converters.ConversionException;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.ConverterLookup;
-import com.thoughtworks.xstream.converters.basic.NullConverter;
+import com.thoughtworks.xstream.converters.collections.*;
+import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
+import com.thoughtworks.xstream.converters.reflection.ReflectionProvider;
+import com.thoughtworks.xstream.converters.basic.*;
+import com.thoughtworks.xstream.alias.ClassMapper;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class DefaultConverterLookup implements ConverterLookup {
 
     private LinkedList converters = new LinkedList();
     private Converter nullConverter = new NullConverter();
     private HashMap typeToConverterMap = new HashMap();
+    private ReflectionProvider reflectionProvider;
+    private ClassMapper classMapper;
+    private String classAttributeIdentifier;
+
+    public DefaultConverterLookup(ReflectionProvider reflectionProvider,
+                                  ClassMapper classMapper,
+                                  String classAttributeIdentifier) {
+        this.reflectionProvider = reflectionProvider;
+        this.classMapper = classMapper;
+        this.classAttributeIdentifier = classAttributeIdentifier;
+    }
 
     public Converter lookupConverterForType(Class type) {
         if (type == null) {
@@ -33,6 +46,71 @@ public class DefaultConverterLookup implements ConverterLookup {
 
     public void registerConverter(Converter converter) {
         converters.addFirst(converter);
+    }
+
+    public void setupDefaults() {
+        alias("int", Integer.class);
+        alias("float", Float.class);
+        alias("double", Double.class);
+        alias("long", Long.class);
+        alias("short", Short.class);
+        alias("char", Character.class);
+        alias("byte", Byte.class);
+        alias("boolean", Boolean.class);
+        alias("number", Number.class);
+        alias("object", Object.class);
+
+        alias("string-buffer", StringBuffer.class);
+        alias("string", String.class);
+        alias("java-class", Class.class);
+        alias("date", Date.class);
+        alias("bit-set", BitSet.class);
+
+        alias("map", Map.class, HashMap.class);
+        alias("properties", Properties.class);
+        alias("list", List.class, ArrayList.class);
+        alias("set", Set.class, HashSet.class);
+
+        alias("linked-list", LinkedList.class);
+        alias("vector", Vector.class);
+        alias("tree-map", TreeMap.class);
+        alias("tree-set", TreeSet.class);
+        alias("hashtable", Hashtable.class);
+
+        registerConverter(new ReflectionConverter(classMapper,classAttributeIdentifier, reflectionProvider));
+
+        registerConverter(new IntConverter());
+        registerConverter(new FloatConverter());
+        registerConverter(new DoubleConverter());
+        registerConverter(new LongConverter());
+        registerConverter(new ShortConverter());
+        registerConverter(new CharConverter());
+        registerConverter(new BooleanConverter());
+        registerConverter(new ByteConverter());
+
+        registerConverter(new StringConverter());
+        registerConverter(new StringBufferConverter());
+        registerConverter(new DateConverter());
+        registerConverter(new JavaClassConverter());
+        registerConverter(new BitSetConverter());
+
+        registerConverter(new ArrayConverter(classMapper,classAttributeIdentifier));
+        registerConverter(new CharArrayConverter());
+        registerConverter(new CollectionConverter(classMapper,classAttributeIdentifier));
+        registerConverter(new MapConverter(classMapper,classAttributeIdentifier));
+        registerConverter(new PropertiesConverter());
+    }
+
+    public void alias(String elementName, Class type, Class defaultImplementation) {
+        classMapper.alias(elementName, type, defaultImplementation);
+    }
+
+    public void alias(String elementName, Class type) {
+        alias(elementName, type, type);
+    }
+
+    public String getClassAttributeIdentifier() {
+        return classAttributeIdentifier;
     }
 
 }
