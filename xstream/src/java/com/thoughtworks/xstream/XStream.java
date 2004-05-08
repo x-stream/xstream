c22f1ca2a6937fcd2f794740589703ab4fee3187
Joe Walnes <joe@truemesh.com>
2004-05-08 22:58:13 +0000

    JavaDoc
    git-svn-id: http://svn.codehaus.org/xstream/trunk@190 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/XStream.java
+++ xstream/src/java/com/thoughtworks/xstream/XStream.java
@@ -17,6 +17,78 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 
+/**
+ * Simple facade to XStream library, a Java-XML serialization tool.
+ * <p/>
+ * <p><hr><b>Example</b><blockquote><pre>
+ * XStream xstream = new XStream();
+ * String xml = xstream.toXML(myObject); // serialize to XML
+ * Object myObject2 = xstream.fromXML(xml); // deserialize from XML
+ * </pre></blockquote><hr>
+ * <p/>
+ * <h3>Aliasing classes</h3>
+ * <p/>
+ * <p>To create shorter XML, you can specify aliases for classes using
+ * the <code>alias()</code> method.
+ * For example, you can shorten all occurences of element
+ * <code>&lt;com.blah.MyThing&gt;</code> to
+ * <code>&lt;my-thing&gt;</code> by registering an alias for the class.
+ * <p><hr><blockquote><pre>
+ * xstream.alias("my-thing", MyThing.class);
+ * </pre></blockquote><hr>
+ * <p/>
+ * <h3>Converters</h3>
+ * <p/>
+ * <p>XStream contains a map of {@link com.thoughtworks.xstream.converters.Converter}
+ * instances, each of which acts as a strategy for converting a particular type
+ * of class to XML and back again. Out of the box, XStream contains converters
+ * for most basic types (String, Date, int, boolean, etc) and collections (Map, List,
+ * Set, Properties, etc). For other objects reflection is used to serialize
+ * each field recursively.</p>
+ * <p/>
+ * <p>Extra converters can be registered using the <code>registerConverter()</code>
+ * method. Some non-standard converters are supplied in the
+ * {@link com.thoughtworks.xstream.converters.extended} package and you can create
+ * your own by implementing the {@link com.thoughtworks.xstream.converters.Converter}
+ * interface.</p>
+ * <p/>
+ * <p><hr><b>Example</b><blockquote><pre>
+ * xstream.registerConverter(new SqlTimestampConverter());
+ * xstream.registerConverter(new DynamicProxyConverter());
+ * </pre></blockquote><hr>
+ * <p/>
+ * <h3>Object graphs</h3>
+ * <p/>
+ * <p>XStream has support for object graphs; a deserialized object graph
+ * will keep references intact, including circular references.</p>
+ * <p/>
+ * <p>XStream can signify references in XML using either XPath or IDs. The
+ * mode can be changed using <code>setMode()</code>:</p>
+ * <p/>
+ * <table border="1">
+ * <tr>
+ * <td><code>xstream.setMode(XStream.XPATH_REFERENCES);</code></td>
+ * <td><i>(Default)</i> Uses XPath references to signify duplicate
+ * references. This produces XML with the least clutter.</td>
+ * </tr>
+ * <tr>
+ * <td><code>xstream.setMode(XStream.ID_REFERENCES);</code></td>
+ * <td>Uses ID references to signify duplicate references. In some
+ * scenarios, such as when using hand-written XML, this is
+ * easier to work with.</td>
+ * </tr>
+ * <tr>
+ * <td><code>xstream.setMode(XStream.NO_REFERENCES);</code></td>
+ * <td>This disables object graph support and treats the object
+ * structure like a tree. Duplicate references are treated
+ * as two seperate objects and circular references cause an
+ * exception. This is slightly faster and uses less memory
+ * than the other two modes.</td>
+ * </tr>
+ * </table>
+ *
+ * @author Joe Walnes
+ */
 public class XStream {
 
     private HierarchicalStreamDriver hierarchicalStreamDriver;
