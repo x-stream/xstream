ad59ef15e6b73742f970d3248acc6e60c9da038b
Joe Walnes <joe@truemesh.com>
2004-05-29 11:11:18 +0000

    DC to build and deploy snapshot on each checkin
    git-svn-id: http://svn.codehaus.org/xstream/trunk@241 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/continuous-integration/build-website.sh
+++ xstream/continuous-integration/build-website.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ant clean website
+ant clean dist
 tar -C build/website -cf - . | ssh beaver tar -C /home/projects/xstream/public_html -xf -
+scp build/xstream-SNAPSHOT.zip beaver:/home/projects/xstream/dist/distributions/
+scp build/xstream-SNAPSHOT.jar beaver:/home/projects/xstream/dist/jars/
