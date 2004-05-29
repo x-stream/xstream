2a327ec31fdb2019e33e2fd770b9fc4e22ffdee0
Joe Walnes <joe@truemesh.com>
2004-05-29 11:16:40 +0000

    Wake up dc
    git-svn-id: http://svn.codehaus.org/xstream/trunk@242 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/continuous-integration/build-website.sh
+++ xstream/continuous-integration/build-website.sh
@@ -4,3 +4,4 @@ ant clean dist
 tar -C build/website -cf - . | ssh beaver tar -C /home/projects/xstream/public_html -xf -
 scp build/xstream-SNAPSHOT.zip beaver:/home/projects/xstream/dist/distributions/
 scp build/xstream-SNAPSHOT.jar beaver:/home/projects/xstream/dist/jars/
+
