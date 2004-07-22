b5ef348b0b958aad68fc9c2a847d88fa353fb0e6
Joe Walnes <joe@truemesh.com>
2004-07-22 18:54:31 +0000

    Bugfix XSTR-87: Cannot deserialize fields that contain $ char as they are invalid XML elements.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@289 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/core/DefaultClassMapper.java
+++ xstream/src/java/com/thoughtworks/xstream/core/DefaultClassMapper.java
@@ -28,11 +28,39 @@ public class DefaultClassMapper implements ClassMapper {
     }
 
     public String mapNameToXML(String javaName) {
-        return javaName;
+        StringBuffer result = new StringBuffer();
+        int length = javaName.length();
+        for(int i = 0; i < length; i++) {
+            char c = javaName.charAt(i);
+            if (c == '$') {
+                result.append("_DOLLAR_");
+            } else if (c == '_') {
+                result.append("__");
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 
     public String mapNameFromXML(String xmlName) {
-        return xmlName;
+        StringBuffer result = new StringBuffer();
+        int length = xmlName.length();
+        for(int i = 0; i < length; i++) {
+            char c = xmlName.charAt(i);
+            if (c == '_') {
+                if (xmlName.charAt(i + 1)  == '_') {
+                    i++;
+                    result.append('_');
+                } else if (xmlName.substring(i + 1, i + 8).equals("DOLLAR_")) {
+                    i += 7;
+                    result.append('$');
+                }
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 
     public void alias(String elementName, Class type, Class defaultImplementation) {
---
+++ xstream/src/test/com/thoughtworks/acceptance/AwkwardCharactersTest.java
@@ -0,0 +1,57 @@
+package com.thoughtworks.acceptance;
+
+public class AwkwardCharactersTest extends AbstractAcceptanceTest {
+
+    public static class WithDollarCharField extends StandardObject {
+        String $field;
+        String field$;
+        String fi$eld;
+        String fi$$eld;
+    }
+
+    public void testSupportsFieldsWithDollarChar() {
+        xstream.alias("dollar", WithDollarCharField.class);
+
+        WithDollarCharField in = new WithDollarCharField();
+        in.$field = "a";
+        in.field$ = "b";
+        in.fi$eld = "c";
+        in.fi$$eld = "d";
+
+        String expected = "" +
+                "<dollar>\n" +
+                "  <_DOLLAR_field>a</_DOLLAR_field>\n" +
+                "  <field_DOLLAR_>b</field_DOLLAR_>\n" +
+                "  <fi_DOLLAR_eld>c</fi_DOLLAR_eld>\n" +
+                "  <fi_DOLLAR__DOLLAR_eld>d</fi_DOLLAR__DOLLAR_eld>\n" +
+                "</dollar>";
+        assertBothWays(in, expected);
+    }
+
+    public static class WithUnderscoreCharField extends StandardObject {
+        String _field;
+        String field_;
+        String fi_eld;
+        String fi__eld;
+    }
+
+    public void testSupportsFieldsWithUnderscoreChar() {
+        xstream.alias("underscore", WithUnderscoreCharField.class);
+
+        WithUnderscoreCharField in = new WithUnderscoreCharField();
+        in._field = "a";
+        in.field_ = "b";
+        in.fi_eld = "c";
+        in.fi__eld = "d";
+
+        String expected = "" +
+                "<underscore>\n" +
+                "  <__field>a</__field>\n" +
+                "  <field__>b</field__>\n" +
+                "  <fi__eld>c</fi__eld>\n" +
+                "  <fi____eld>d</fi____eld>\n" +
+                "</underscore>";
+        assertBothWays(in, expected);
+    }
+
+}
