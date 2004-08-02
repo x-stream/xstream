08c1c6749f71d4bbbc76c9d7396d953918a95d00
Joe Walnes <joe@truemesh.com>
2004-08-02 19:57:04 +0000

    More hacking at base64
    git-svn-id: http://svn.codehaus.org/xstream/trunk@313 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/extended/base64/Base64Encoder.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/base64/Base64Encoder.java
@@ -7,32 +7,34 @@ package com.thoughtworks.xstream.converters.extended.base64;
  */
 public class Base64Encoder {
 
-    public char[] tripleByteToQuadChar(byte[] bytes) {
+    private static final char[] sixtyFourChars
+            = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
+
+    public String encode(byte[] input) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < input.length; i += 3) {
+            byte[] subset = new byte[Math.min(3, input.length - i)];
+            for (int s = 0; s < subset.length; s++) {
+                subset[s] = input[i + s];
+            }
+            result.append(tripleByteToQuadChar(subset));
+        }
+        return result.toString();
+    }
+
+    private char[] tripleByteToQuadChar(byte[] in) {
         // input  |------||------||------| (3 values each with 8 bits)
         //        101010101010101010101010
         // output |----||----||----||----| (4 values each with 6 bits)
-        char[] result = new char[4];
-        result[0] = charOfBase64Number(bytes[0]+256 >> 2);
-        result[1] = charOfBase64Number((bytes[0]+256 << 4 & 48) + (bytes[1]+256 >> 4));
-        result[2] = charOfBase64Number(((bytes[1]+256 & 15) << 2) + (bytes[2]+256 >> 6));
-        result[3] = charOfBase64Number(bytes[2]+256 & 63);
-        return result;
-    }
-
-    public char charOfBase64Number(int number) {
-        if (number < 0 || number > 63) {
-            throw new IllegalArgumentException("Value must be in range 0-63. Was: " + number);
-        } else if (number < 26) {
-            return (char)(number + 'A');
-        } else if (number < 52) {
-            return (char)(number - 26 + 'a');
-        } else if (number < 62) {
-            return (char)(number - 52 + '0');
-        } else if (number == 62) {
-            return '+';
-        } else {
-            return '/';
+        if (in.length > 3) {
+            throw new Error();
         }
+        int oneBigNumber = (in[0] << 16) | ((in.length > 1 ? in[1] : 0) << 8) | ((in.length > 2 ? in[2] : 0));
+        char[] result = new char[4];
+        for (int i = 0; i < 4; i++) {
+            result[i] = in.length + 1 == i ? '=' : sixtyFourChars[63 & (oneBigNumber >> (6 * (3 - i)))];
+        }
+        return result;
     }
 
 }
