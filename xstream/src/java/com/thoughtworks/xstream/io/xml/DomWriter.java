53bfe37da2fd16b8902a612a2cf44b61116c10a6
Joe Walnes <joe@truemesh.com>
2004-09-02 07:35:47 +0000

    XSTR-108: Added DomWriter (contributed by Michael Kopp)
    git-svn-id: http://svn.codehaus.org/xstream/trunk@349 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/java/com/thoughtworks/xstream/io/xml/DomWriter.java
@@ -0,0 +1,45 @@
+package com.thoughtworks.xstream.io.xml;
+
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * @author Michael Kopp
+ */
+public class DomWriter implements HierarchicalStreamWriter {
+    private final Document document;
+    private Element current;
+
+    public DomWriter(Document document) {
+        this.document = document;
+        this.current = document.getDocumentElement();
+    }
+
+    public DomWriter(Element rootElement) {
+        document = rootElement.getOwnerDocument();
+        current = rootElement;
+    }
+
+    public void startNode(String name) {
+        final Element child = document.createElement(name);
+        if (current == null) {
+            document.appendChild(child);
+        } else {
+            current.appendChild(child);
+        }
+        current = child;
+    }
+
+    public void addAttribute(String name, String value) {
+        current.setAttribute(name, value);
+    }
+
+    public void setValue(String text) {
+        current.appendChild(document.createTextNode(text));
+    }
+
+    public void endNode() {
+        current = (Element) current.getParentNode();
+    }
+}
