517fa2bb61e9dc3aedbc7280ea63c9533b7ccb36
Mauro Talevi <mauro.talevi@aquilonia.org>
2004-12-19 14:55:43 +0000

    Fixed test - following change in DateConverter. The standard format now has the timezone and the test failed in one of the two ways of serialisation.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@411 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/acceptance/BasicTypesTest.java
+++ xstream/src/test/com/thoughtworks/acceptance/BasicTypesTest.java
@@ -43,7 +43,7 @@ public class BasicTypesTest extends AbstractAcceptanceTest {
 
     public void testDate() {
         Date date = new Date(103, 02, 15, 8, 22, 7);
-        assertBothWays(date, "<date>2003-03-15 08:22:07.0 AM</date>");
+        assertBothWays(date, "<date>2003-03-15 08:22:07.0 GMT</date>");
     }
 
     public void testBigInteger() {
