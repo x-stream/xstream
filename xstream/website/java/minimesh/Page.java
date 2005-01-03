0ccd440a7b465a8ae0c5a929cfcf578bed3fec37
Joe Walnes <joe@truemesh.com>
2005-01-03 22:56:34 +0000

    Upgraded to latest sitemesh snapshot - decoupled offline website from servlet api
    git-svn-id: http://svn.codehaus.org/xstream/trunk@421 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/website/java/minimesh/Page.java
+++ xstream/website/java/minimesh/Page.java
@@ -1,10 +1,16 @@
 package minimesh;
 
-import com.opensymphony.module.sitemesh.HTMLPage;
-import com.opensymphony.module.sitemesh.parser.HTMLPageParser;
+import com.opensymphony.module.sitemesh.html.HTMLProcessor;
+import com.opensymphony.module.sitemesh.html.util.CharArray;
+import com.opensymphony.module.sitemesh.parser.PageBuilder;
+import com.opensymphony.module.sitemesh.parser.rules.BodyTagRule;
+import com.opensymphony.module.sitemesh.parser.rules.HeadExtractingRule;
+import com.opensymphony.module.sitemesh.parser.rules.TitleExtractingRule;
+import com.opensymphony.module.sitemesh.parser.rules.MetaTagRule;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * A single page in a website, including title, filename and content.
@@ -15,35 +21,71 @@ import java.io.IOException;
  */
 public class Page {
 
-    private final HTMLPageParser htmlPageParser = new HTMLPageParser();
-    private final FileSystem fileSystem = new FileSystem();
-
-    private final HTMLPage sitemeshPage;
-    private final String filename;
+    private Properties properties;
+    private String filename;
+    private String head;
+    private String body;
 
     public Page(File htmlFile) {
         try {
-            sitemeshPage = (HTMLPage) htmlPageParser.parse(fileSystem.readFile(htmlFile));
             filename = htmlFile.getName();
+            FileSystem fileSystem = new FileSystem();
+            char[] rawHTML = fileSystem.readFile(htmlFile);
+            extractContentFromHTML(rawHTML);
         } catch (IOException e) {
             throw new CannotParsePageException(e);
         }
     }
 
+    public Page(String filename, String htmlContent) {
+        try {
+            this.filename = filename;
+            extractContentFromHTML(htmlContent.toCharArray());
+        } catch (IOException e) {
+            throw new CannotParsePageException(e);
+        }
+    }
+
+    private void extractContentFromHTML(char[] rawHTML) throws IOException {
+        // where to dump properties extracted from the page
+        properties = new Properties();
+        PageBuilder pageBuilder = new PageBuilder() {
+            public void addProperty(String key, String value) {
+                properties.setProperty(key, value);
+            }
+        };
+
+        // buffers to hold head and body content
+        CharArray head = new CharArray(64);
+        CharArray body = new CharArray(4096);
+
+        // setup rules for html processor
+        HTMLProcessor htmlProcessor = new HTMLProcessor(rawHTML, body);
+        htmlProcessor.addRule(new BodyTagRule(pageBuilder, body));
+        htmlProcessor.addRule(new HeadExtractingRule(head));
+        htmlProcessor.addRule(new TitleExtractingRule(pageBuilder));
+        htmlProcessor.addRule(new MetaTagRule(pageBuilder));
+
+        // go!
+        htmlProcessor.process();
+        this.head = head.toString();
+        this.body = body.toString();
+    }
+
     public String getTitle() {
-        if (sitemeshPage.isPropertySet("meta.short")) {
-            return sitemeshPage.getProperty("meta.short");
+        if (properties.containsKey("meta.short")) {
+            return properties.getProperty("meta.short");
         } else {
-            return sitemeshPage.getTitle();
+            return properties.getProperty("title");
         }
     }
 
     public String getHead() {
-        return sitemeshPage.getHead();
+        return head.toString();
     }
 
     public String getBody() {
-        return sitemeshPage.getBody();
+        return body.toString();
     }
 
     public String getFilename() {
