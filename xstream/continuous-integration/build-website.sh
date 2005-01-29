514a296d8bc65e1172d482cf98e5cc8125760ee1
Joe Walnes <joe@truemesh.com>
2005-01-29 12:35:02 +0000

    Split build.xml into two separate builds: one for the xstream library and one for the website. This simplifies the build files greatly and means the user doesn't have a buildfile of stuff not related to the download.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@478 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/continuous-integration/build-website.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ant all
-tar -C build/website -cf - . | ssh beaver tar -C /home/projects/xstream/public_html -xf -
-scp build/xstream-SNAPSHOT.zip beaver:/home/projects/xstream/dist/distributions/
-scp build/xstream-SNAPSHOT.jar beaver:/home/projects/xstream/dist/jars/
-
