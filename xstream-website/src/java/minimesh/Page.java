56f6c13feb7c691480f958c90b6c222c71a66026
Joe Walnes <joe@truemesh.com>
2005-01-29 13:25:09 +0000

    optimized imports
    git-svn-id: http://svn.codehaus.org/xstream/trunk@482 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream-website/src/java/minimesh/Page.java
+++ xstream-website/src/java/minimesh/Page.java
@@ -1,24 +1,21 @@
 package minimesh;
 
-import com.opensymphony.module.sitemesh.html.HTMLProcessor;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Properties;
+
 import com.opensymphony.module.sitemesh.html.BasicRule;
+import com.opensymphony.module.sitemesh.html.HTMLProcessor;
 import com.opensymphony.module.sitemesh.html.Tag;
 import com.opensymphony.module.sitemesh.html.util.CharArray;
 import com.opensymphony.module.sitemesh.parser.PageBuilder;
 import com.opensymphony.module.sitemesh.parser.rules.BodyTagRule;
 import com.opensymphony.module.sitemesh.parser.rules.HeadExtractingRule;
-import com.opensymphony.module.sitemesh.parser.rules.TitleExtractingRule;
 import com.opensymphony.module.sitemesh.parser.rules.MetaTagRule;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Collection;
-import java.util.Collections;
+import com.opensymphony.module.sitemesh.parser.rules.TitleExtractingRule;
 
 /**
  * A single page in a website, including title, filename and content.
@@ -65,21 +62,21 @@ public class Page {
         };
 
         // buffers to hold head and body content
-        CharArray head = new CharArray(64);
-        CharArray body = new CharArray(4096);
+        CharArray headBuffer = new CharArray(64);
+        CharArray bodyBuffer = new CharArray(4096);
 
         // setup rules for html processor
-        HTMLProcessor htmlProcessor = new HTMLProcessor(rawHTML, body);
-        htmlProcessor.addRule(new BodyTagRule(pageBuilder, body));
-        htmlProcessor.addRule(new HeadExtractingRule(head));
+        HTMLProcessor htmlProcessor = new HTMLProcessor(rawHTML, bodyBuffer);
+        htmlProcessor.addRule(new BodyTagRule(pageBuilder, bodyBuffer));
+        htmlProcessor.addRule(new HeadExtractingRule(headBuffer));
         htmlProcessor.addRule(new TitleExtractingRule(pageBuilder));
         htmlProcessor.addRule(new MetaTagRule(pageBuilder));
         htmlProcessor.addRule(new LinkExtractingRule());
 
         // go!
         htmlProcessor.process();
-        this.head = head.toString();
-        this.body = body.toString();
+        this.head = headBuffer.toString();
+        this.body = bodyBuffer.toString();
     }
 
     public String getTitle() {
