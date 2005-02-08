2e7d2454f9dbe80d259e55e7fb23528b8ef2ad98
Joe Walnes <joe@truemesh.com>
2005-02-08 20:48:37 +0000

    Rolled back a println that I didn't mean to commit
    git-svn-id: http://svn.codehaus.org/xstream/trunk@506 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/reflection/SerializationMethodInvoker.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/reflection/SerializationMethodInvoker.java
@@ -83,7 +83,6 @@ public class SerializationMethodInvoker {
     public void callWriteObject(Class type, Object instance, ObjectOutputStream stream) {
         try {
             Method readObjectMethod = getMethod(type, "writeObject", new Class[]{ObjectOutputStream.class}, false);
-            System.out.println(type.getName());
             readObjectMethod.invoke(instance, new Object[]{stream});
         } catch (IllegalAccessException e) {
             throw new ConversionException("Could not call " + instance.getClass().getName() + ".writeObject()", e);
