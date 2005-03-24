5e9d1c61173764c7712c5f4076faeb2848fe014d
Joe Walnes <joe@truemesh.com>
2005-03-24 07:14:38 +0000

    PropertiesConverter also serializes default values (thanks to Kevin Ring)
    git-svn-id: http://svn.codehaus.org/xstream/trunk@536 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/collections/PropertiesConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/collections/PropertiesConverter.java
@@ -3,12 +3,14 @@ package com.thoughtworks.xstream.converters.collections;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.converters.ConversionException;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
+import java.lang.reflect.Field;
 
 /**
  * Special converter for java.util.Properties that stores
@@ -17,10 +19,23 @@ import java.util.Properties;
  * <p>Because all entries of a Properties instance
  * are Strings, a single element is used for each property
  * with two attributes; one for key and one for value.</p>
+ * <p>Additionally, default properties are also serialized, if they are present.</p>
  *
  * @author Joe Walnes
+ * @author Kevin Ring
  */
 public class PropertiesConverter implements Converter {
+
+    private static Field defaultsField;
+
+    static {
+        try {
+            defaultsField = Properties.class.getDeclaredField("defaults");
+            defaultsField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            defaultsField = null;
+        }
+    }
 
     public boolean canConvert(Class type) {
         return Properties.class == type;
@@ -35,18 +50,51 @@ public class PropertiesConverter implements Converter {
             writer.addAttribute("value", entry.getValue().toString());
             writer.endNode();
         }
+        Properties defaults = getDefaults(properties);
+        if (defaults != null) {
+            writer.startNode("defaults");
+            marshal(defaults, writer, context);
+            writer.endNode();
+        }
     }
 
     public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
         Properties properties = new Properties();
         while (reader.hasMoreChildren()) {
             reader.moveDown();
-            String name = reader.getAttribute("name");
-            String value = reader.getAttribute("value");
-            ;
-            properties.setProperty(name, value);
+            if (reader.getNodeName().equals("defaults")) {
+                Properties defaults = (Properties) unmarshal(reader, context);
+                setDefaults(properties, defaults);
+            } else {
+                String name = reader.getAttribute("name");
+                String value = reader.getAttribute("value");
+                properties.setProperty(name, value);
+            }
             reader.moveUp();
         }
         return properties;
     }
+
+    private Properties getDefaults(Properties properties) {
+        if (defaultsField == null) {
+            throw new ConversionException("Could not access java.util.Properties.defaults field");
+        }
+        try {
+            return (Properties) defaultsField.get(properties);
+        } catch (IllegalAccessException e) {
+            throw new ConversionException("Could not read java.util.Properties.default field");
+        }
+    }
+
+    private void setDefaults(Properties properties, Properties defaults) {
+        if (defaultsField == null) {
+            throw new ConversionException("Could not access java.util.Properties.defaults field");
+        }
+        try {
+            defaultsField.set(properties, defaults);
+        } catch (IllegalAccessException e) {
+            throw new ConversionException("Could not write java.util.Properties.defaults field");
+        }
+    }
+
 }
--- xstream/src/test/com/thoughtworks/xstream/converters/collections/PropertiesConverterTest.java
+++ xstream/src/test/com/thoughtworks/xstream/converters/collections/PropertiesConverterTest.java
@@ -13,23 +13,53 @@ public class PropertiesConverterTest extends TestCase {
         in.setProperty("hello", "world");
         in.setProperty("foo", "cheese");
 
-        XStream xStream = new XStream(new XppDriver());
-
         String expectedXML = "" +
                 "<properties>\n" +
                 "  <property name=\"hello\" value=\"world\"/>\n" +
                 "  <property name=\"foo\" value=\"cheese\"/>\n" +
                 "</properties>";
-        String actualXML = xStream.toXML(in);
+        XStream xstream = new XStream();
+        String actualXML = xstream.toXML(in);
         assertEquals(expectedXML, actualXML);
 
         Properties expectedOut = new Properties();
         expectedOut.setProperty("hello", "world");
         expectedOut.setProperty("foo", "cheese");
-        Properties actualOut = (Properties) xStream.fromXML(actualXML);
+        Properties actualOut = (Properties) xstream.fromXML(actualXML);
         assertEquals(in, actualOut);
         assertEquals(in.toString(), actualOut.toString());
 
+    }
+
+    public void testIncludesDefaultProperties() {
+        Properties defaults = new Properties();
+        defaults.setProperty("host", "localhost");
+        defaults.setProperty("port", "80");
+
+        Properties override = new Properties(defaults);
+        override.setProperty("port", "999");
+
+        // sanity check
+        assertEquals("Unexpected overriden property", "999", override.getProperty("port"));
+        assertEquals("Unexpected default property", "localhost", override.getProperty("host"));
+
+        String expectedXML = "" +
+                "<properties>\n" +
+                "  <property name=\"port\" value=\"999\"/>\n" +
+                "  <defaults>\n" +
+                "    <property name=\"port\" value=\"80\"/>\n" +
+                "    <property name=\"host\" value=\"localhost\"/>\n" +
+                "  </defaults>\n" +
+                "</properties>";
+
+        XStream xstream = new XStream();
+        String actualXML = xstream.toXML(override);
+        assertEquals(expectedXML, actualXML);
+
+        Properties out = (Properties) xstream.fromXML(actualXML);
+        assertEquals("Unexpected overriden property", "999", out.getProperty("port"));
+        assertEquals("Unexpected default property", "localhost", out.getProperty("host"));
+        assertEquals(override, out);
     }
 
 }
