7b8fcfe11e0a4519f51281411e683e6ea78e25d3
James Strachan <james.strachan@gmail.com>
2005-04-25 13:34:38 +0000

    Added a new explicit test case to catch any empty-byte array related bugs
    git-svn-id: http://svn.codehaus.org/xstream/trunk@573 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/test/com/thoughtworks/xstream/converters/collections/ByteArrayConverterTest.java
@@ -0,0 +1,58 @@
+package com.thoughtworks.xstream.converters.collections;
+
+import com.thoughtworks.acceptance.AbstractAcceptanceTest;
+
+public class ByteArrayConverterTest extends AbstractAcceptanceTest {
+
+
+    public void testMarshallByteArrays() {
+        Dummy input = new Dummy(new byte[0]);
+
+        String expected = "<dummy>\n  <data></data>\n</dummy>";
+        assertBothWays(input, expected);
+    }
+
+    public static class Dummy {
+        byte[] data;
+
+        public Dummy(byte[] data) {
+            this.data = data;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
+
+        public boolean equals(Object that) {
+            if (that instanceof Dummy) {
+                return equals((Dummy) that);
+            }
+            return false;
+        }
+
+        public boolean equals(Dummy that) {
+            if (this.data == that.data) {
+                return true;
+            }
+            if (this.data != null && that.data != null) {
+                if (this.data.length == that.data.length) {
+                    for (int i = 0; i < data.length; i++) {
+                        byte b1 = data[i];
+                        byte b2 = that.data[i];
+                        if (b1 != b2) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            return false;
+
+        }
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        xstream.alias("dummy", Dummy.class);
+    }
+}
