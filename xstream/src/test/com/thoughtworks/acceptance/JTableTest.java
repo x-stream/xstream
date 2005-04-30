b971de85b1bf4dcea8359c0cb1391c703f0544a3
Joe Walnes <joe@truemesh.com>
2005-04-30 08:41:20 +0000

    Added simple test for JTable. JTable is one of the nastiest components to serialize. If this works, we're in good shape.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@587 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/test/com/thoughtworks/acceptance/JTableTest.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.acceptance;
+
+import javax.swing.*;
+
+public class JTableTest extends AbstractAcceptanceTest {
+
+    // JTable is one of the nastiest components to serialize. If this works, we're in good shape :)
+
+    public void testJTable() {
+        // Note: JTable does not have a sensible .equals() method, so we compare the XML instead.
+
+        JTable original = new JTable();
+        String originalXml = xstream.toXML(original);
+
+        JTable deserialized = (JTable) xstream.fromXML(originalXml);
+        String deserializedXml = xstream.toXML(deserialized);
+
+        assertEquals(originalXml, deserializedXml);
+    }
+
+}
