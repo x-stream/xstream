5bcd458c6c23c11318d5332b2d9d57edc71a6d8e
Jörg Schaible <joerg.schaible@gmx.de>
2005-10-03 01:31:01 +0000

    Fix jdk 1.3 issue.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@623 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/extended/ISO8601SqlTimestampConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/ISO8601SqlTimestampConverter.java
@@ -3,9 +3,14 @@ package com.thoughtworks.xstream.converters.extended;
 import java.sql.Timestamp;
 import java.util.Date;
 
+
 /**
  * A SqlTimestampConverter conforming to the ISO8601 standard.
  * http://www.iso.ch/iso/en/CatalogueDetailPage.CatalogueDetail?CSNUMBER=26780
+ * <p>
+ * Note, that this converter cannot handle the nano and micro part of the Timestamp.
+ * Any value is truncated at milliseconds level.
+ * </p>
  * 
  * @author J&ouml;rg Schaible
  * @since 1.2
@@ -17,7 +22,18 @@ public class ISO8601SqlTimestampConverter extends ISO8601DateConverter {
     }
 
     protected Object fromString(String str) {
-        return new Timestamp(((Date)super.fromString(str)).getTime());
+        // special nano handling required for JDK 1.3
+        final Date date = (Date)super.fromString(str);
+        final Timestamp timestamp = new Timestamp((date.getTime() / 1000) * 1000);
+        timestamp.setNanos((int)((date.getTime() % 1000) * 1000000));
+        return timestamp;
+    }
+
+    protected String toString(Object obj) {
+        // special nano handling required for JDK 1.3
+        final Timestamp timestamp = (Timestamp)obj;
+        return super.toString(new Date(((timestamp.getTime() / 1000) * 1000)
+                + (timestamp.getNanos() / 1000000)));
     }
 
 }
--- xstream/src/test/com/thoughtworks/xstream/converters/extended/ISO8601SqlTimestampConverterTest.java
+++ xstream/src/test/com/thoughtworks/xstream/converters/extended/ISO8601SqlTimestampConverterTest.java
@@ -10,9 +10,8 @@ import com.thoughtworks.xstream.converters.extended.ISO8601SqlTimestampConverter
 import junit.framework.TestCase;
 
 /**
- * Created Jan 16, 2005
- * 
  * @author Chung-Onn Cheong
+ * @author J&ouml;rg Schaible
  */
 public class ISO8601SqlTimestampConverterTest extends TestCase {
 
@@ -25,13 +24,11 @@ public class ISO8601SqlTimestampConverterTest extends TestCase {
         
         Timestamp ts1 = new Timestamp(currentTime); 
         String xmlString = xs.toXML(ts1);
-        System.out.println("ISO8601 xmlString:"+ xmlString);
         
         Timestamp ts2 = (Timestamp) xs.fromXML(xmlString);
-        System.out.println("ISO8601 timestamp:"+ts2);
         
         assertEquals("ISO Timestamp Converted is not the same ",ts1,ts2);
-        assertEquals("Current time not equal to converted timestamp", currentTime, ts2.getTime());
+        assertEquals("Current time not equal to converted timestamp", currentTime, ts2.getTime() / 1000 * 1000 + ts2.getNanos() / 1000000);
     }
     
 }
