fce41daa0b784da3eac37b89f9ae9023da850be5
Jörg Schaible <joerg.schaible@gmx.de>
2005-11-22 22:52:05 +0000

    Fix unit test ISO8601GregorianCalenderConverter for JDK 1.3.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@645 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/converters/extended/ISO8601GregorianCalendarConverterTest.java
+++ xstream/src/test/com/thoughtworks/xstream/converters/extended/ISO8601GregorianCalendarConverterTest.java
@@ -6,6 +6,8 @@ import com.thoughtworks.xstream.testutil.TimeZoneChanger;
 
 import junit.framework.TestCase;
 
+import org.joda.time.DateTime;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -46,17 +48,14 @@ public class ISO8601GregorianCalendarConverterTest extends TestCase {
     
     public void testSavedTimeIsInUTC() {
         Calendar in = Calendar.getInstance();
-        String simpleDateFormatString = "yyyy-MM-dd'T'HH:mm:ss.SSS";
         final String iso8601;
         if (JVM.is14()) {
-            SimpleDateFormat format = new SimpleDateFormat(simpleDateFormatString + "Z");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
             String jdkConverted = format.format(in.getTime());
             iso8601 = jdkConverted.substring(0, jdkConverted.length() - 2) + ":" + jdkConverted.substring(jdkConverted.length() - 2);
         } else {
-            // JDK 1.3 can only format with symbolic time zones, hour must match still though ensuring validity
-            SimpleDateFormat format = new SimpleDateFormat(simpleDateFormatString + "z");
-            String jdkConverted = format.format(in.getTime());
-            iso8601 = jdkConverted.substring(0, jdkConverted.length() - 3) + "-04:00";
+            // JDK 1.3 can only format with symbolic time zones, so use Joda - it defaults to ISO
+            iso8601 = new DateTime(in).toString();
         }
         String converterXML =  converter.toString(in);
         assertEquals(iso8601, converterXML);
