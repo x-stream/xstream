0de8e1a0a994eca375c1d4a037a7041d379189fa
Jörg Schaible <joerg.schaible@gmx.de>
2006-02-06 08:08:09 +0000

    Fix DecimalFormatSymbols for JDK 1.3.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@663 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/acceptance/EncodingTest.java
+++ xstream/src/test/com/thoughtworks/acceptance/EncodingTest.java
@@ -3,6 +3,8 @@ package com.thoughtworks.acceptance;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
+import com.thoughtworks.xstream.core.JVM;
+
 
 public class EncodingTest extends AbstractAcceptanceTest {
 
@@ -26,8 +28,34 @@ public class EncodingTest extends AbstractAcceptanceTest {
     }
 
     public void testDecimalFormatSymbols() {
-        final DecimalFormatSymbols format = new DecimalFormatSymbols(Locale.GERMANY);
-        assertBothWays(format, "<java.text.DecimalFormatSymbols serialization=\"custom\">\n"
+        final String xml;
+        if (!JVM.is14()) {
+            xml =
+                    "<java.text.DecimalFormatSymbols serialization=\"custom\">\n"
+                + "  <java.text.DecimalFormatSymbols>\n"
+                + "    <default>\n"
+                + "      <decimalSeparator>,</decimalSeparator>\n"
+                + "      <digit>#</digit>\n"
+                + "      <exponential>E</exponential>\n"
+                + "      <groupingSeparator>.</groupingSeparator>\n"
+                + "      <minusSign>-</minusSign>\n"
+                + "      <monetarySeparator>,</monetarySeparator>\n"
+                + "      <patternSeparator>;</patternSeparator>\n"
+                + "      <perMill>\u2030</perMill>\n"
+                + "      <percent>%</percent>\n"
+                + "      <serialVersionOnStream>1</serialVersionOnStream>\n"
+                + "      <zeroDigit>0</zeroDigit>\n"
+                + "      <NaN>\ufffd</NaN>\n"
+                + "      <currencySymbol>DM</currencySymbol>\n"
+                + "      <infinity>\u221e</infinity>\n"
+                + "      <intlCurrencySymbol>DEM</intlCurrencySymbol>\n"
+                + "      <locale>de_DE</locale>\n"
+                + "    </default>\n"
+                + "  </java.text.DecimalFormatSymbols>\n"
+                + "</java.text.DecimalFormatSymbols>";
+        } else {
+            xml =
+                    "<java.text.DecimalFormatSymbols serialization=\"custom\">\n"
                 + "  <java.text.DecimalFormatSymbols>\n"
                 + "    <default>\n"
                 + "      <decimalSeparator>,</decimalSeparator>\n"
@@ -48,6 +76,9 @@ public class EncodingTest extends AbstractAcceptanceTest {
                 + "      <locale>de_DE</locale>\n"
                 + "    </default>\n"
                 + "  </java.text.DecimalFormatSymbols>\n"
-                + "</java.text.DecimalFormatSymbols>");
+                + "</java.text.DecimalFormatSymbols>";
+        }
+        final DecimalFormatSymbols format = new DecimalFormatSymbols(Locale.GERMANY);
+        assertBothWays(format, xml);
     }
 }
