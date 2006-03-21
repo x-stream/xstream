5aa4097e0cddc51b0da8b2704b4db830206faddd
Jörg Schaible <joerg.schaible@gmx.de>
2006-03-21 12:30:14 +0000

    Remove reflection stuff, ThrowableConverter if 1.4 only anyway.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@749 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/extended/ThrowableConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/ThrowableConverter.java
@@ -6,9 +6,6 @@ import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 /**
  * Converter for Throwable (and Exception) that retains stack trace, for JDK1.4 only.
  *
@@ -16,21 +13,6 @@ import java.lang.reflect.Method;
  * @author Joe Walnes
  */
 public class ThrowableConverter implements Converter {
-
-    private final static Method initCause;
-    private final static Method getCause;
-    static {
-        Method init = null;
-        Method get = null;
-        try {
-            init = Throwable.class.getDeclaredMethod("initCause", new Class[]{Throwable.class});
-            get = Throwable.class.getDeclaredMethod("getCause", null);
-        } catch (NoSuchMethodException e) {
-            // JDK 1.3
-        }
-        initCause = init;
-        getCause = get;
-    }
     
     private Converter defaultConverter;
 
@@ -44,18 +26,11 @@ public class ThrowableConverter implements Converter {
 
     public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
         Throwable throwable = (Throwable) source;
-        if (getCause != null) {
+        if (throwable.getCause() == null) {
             try {
-                Throwable cause = (Throwable)getCause.invoke(throwable, null);
-                if (cause == null) {
-                    initCause.invoke(throwable, new Object[]{ null });
-                }
+                throwable.initCause(null);
             } catch (IllegalArgumentException e) {
                 // ignore, initCause failed, cause was already set
-            } catch (IllegalAccessException e) {
-                // ignore
-            } catch (InvocationTargetException e) {
-                // ignore
             }
         }
         throwable.getStackTrace(); // Force stackTrace field to be lazy loaded by special JVM native witchcraft (outside our control).
