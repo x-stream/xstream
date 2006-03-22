1ac83851e4e989b332cab4b62ae4410d7e3afa70
Jörg Schaible <joerg.schaible@gmx.de>
2006-03-22 21:04:40 +0000

    Test DefaultListModel (XSTR-130).
    git-svn-id: http://svn.codehaus.org/xstream/trunk@758 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/test/com/thoughtworks/acceptance/SwingTest.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.acceptance;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JTable;
+
+public class SwingTest extends AbstractAcceptanceTest {
+
+    // JTable is one of the nastiest components to serialize. If this works, we're in good shape :)
+
+    public void testJTable() {
+        // Note: JTable does not have a sensible .equals() method, so we compare the XML instead.
+        JTable original = new JTable();
+        String originalXml = xstream.toXML(original);
+
+        JTable deserialized = (JTable) xstream.fromXML(originalXml);
+        String deserializedXml = xstream.toXML(deserialized);
+
+        assertEquals(originalXml, deserializedXml);
+    }
+
+    public void testDefaultListModel() {
+        final DefaultListModel original = new DefaultListModel();
+        final JList list = new JList();
+        list.setModel(original);
+        
+        String originalXml = xstream.toXML(original);
+        
+        DefaultListModel deserialized = (DefaultListModel) xstream.fromXML(originalXml);
+        String deserializedXml = xstream.toXML(deserialized);
+        
+        assertEquals(originalXml, deserializedXml);
+
+        list.setModel(deserialized);
+    }
+    
+}
