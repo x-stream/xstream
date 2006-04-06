9c3f2d1d6ada537196e5d7f2c0c39d682317f0d9
Jörg Schaible <joerg.schaible@gmx.de>
2006-04-06 23:02:47 +0000

    Add missing CharsetConverter.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@801 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/CharsetConverter.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.xstream.converters.extended;
+
+import com.thoughtworks.xstream.converters.basic.AbstractSingleValueConverter;
+
+import java.nio.charset.Charset;
+
+/**
+ * Converts a java.nio.charset.Carset to a string.
+ * 
+ * @author J&ouml;rg Schaible
+ * @since upcoming
+ */
+public class CharsetConverter extends AbstractSingleValueConverter {
+
+    public boolean canConvert(Class type) {
+        return Charset.class.isAssignableFrom(type);
+    }
+
+    public String toString(Object obj) {
+        return obj == null ? null : ((Charset)obj).name();
+    }
+
+
+    public Object fromString(String str) {
+        return Charset.forName(str);
+    }
+}
