9a2194f5506e8458a0d85c922c4366b217a8e329
Mauro Talevi <mauro.talevi@aquilonia.org>
2006-04-12 14:33:49 +0000

    Made XmlFriendlyMapper replacement strings configurable.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@812 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/mapper/XmlFriendlyMapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/XmlFriendlyMapper.java
@@ -4,19 +4,25 @@ import com.thoughtworks.xstream.alias.ClassMapper;
 
 /**
  * Mapper that ensures that all names in the serialization stream are XML friendly.
- *
- * <b>$</b> (dollar) chars appearing in class names are replaced with <b>_</b> (underscore) chars.<br>
- * <b>$</b> (dollar) chars appearing in field names are replaced with <b>_DOLLAR_</b> string.<br>
- * <b>_</b> (underscore) chars appearing in field names are replaced with <b>__</b> (double underscore) string.<br>
- *
+ * The mapper uses the {@link XmlMapperConfiguration} to allow configuration of the replacement chars in class and field names.
+ * 
  * @author Joe Walnes
+ * @author Mauro Talevi
+ * @see XmlMapperConfiguration
  */
 public class XmlFriendlyMapper extends MapperWrapper {
 
+    private XmlMapperConfiguration configuration;
+    
     public XmlFriendlyMapper(Mapper wrapped) {
-        super(wrapped);
+        this(wrapped, new XmlMapperConfiguration());
     }
 
+    public XmlFriendlyMapper(Mapper wrapped, XmlMapperConfiguration configuration) {
+        super(wrapped);
+        this.configuration = configuration;
+    }
+    
     /**
      * @deprecated As of 1.2, use {@link #XmlFriendlyMapper(Mapper)}
      */
@@ -28,25 +34,24 @@ public class XmlFriendlyMapper extends MapperWrapper {
         String name = super.serializedClass(type);
 
         // the $ used in inner class names is illegal as an xml element getNodeName
-        name = name.replace('$', '-');
+        name = name.replace('$', configuration.dollarReplacementInClass());
 
         // special case for classes named $Blah with no package; <-Blah> is illegal XML
-        if (name.charAt(0) == '-') {
-            name = "default" + name;
+        if (name.charAt(0) == configuration.dollarReplacementInClass()) {
+            name = configuration.noPackagePrefix() + name;
         }
 
         return name;
     }
 
     public Class realClass(String elementName) {
-
         // special case for classes named $Blah with no package; <-Blah> is illegal XML
-        if (elementName.startsWith("default-")) {
-            elementName = elementName.substring(7);
+        if (elementName.startsWith(configuration.noPackagePrefix()+configuration.dollarReplacementInClass())) {
+            elementName = elementName.substring(configuration.noPackagePrefix().length());
         }
 
         // the $ used in inner class names is illegal as an xml element getNodeName
-        elementName = elementName.replace('-', '$');
+        elementName = elementName.replace(configuration.dollarReplacementInClass(), '$');
 
         return super.realClass(elementName);
     }
@@ -72,14 +77,12 @@ public class XmlFriendlyMapper extends MapperWrapper {
         int length = xmlName.length();
         for(int i = 0; i < length; i++) {
             char c = xmlName.charAt(i);
-            if (c == '_') {
-                if (xmlName.charAt(i + 1)  == '_') {
-                    i++;
-                    result.append('_');
-                } else if (xmlName.length() >= i + 8 && xmlName.substring(i + 1, i + 8).equals("DOLLAR_")) {
-                    i += 7;
-                    result.append('$');
-                }
+            if ( stringFoundAt(xmlName, i, configuration.underscoreReplacementInField())) {
+                i += configuration.underscoreReplacementInField().length() - 1;
+                result.append('_');
+            } else if ( stringFoundAt(xmlName, i, configuration.dollarReplacementInField())) {
+                i += configuration.dollarReplacementInField().length() - 1;
+                result.append('$');
             } else {
                 result.append(c);
             }
@@ -87,15 +90,23 @@ public class XmlFriendlyMapper extends MapperWrapper {
         return result.toString();
     }
 
+    private boolean stringFoundAt(String name, int i, String replacement) {
+        if ( name.length() >= i + replacement.length() 
+          && name.substring(i, i + replacement.length()).equals(replacement) ){
+            return true;
+        }
+        return false;
+    }
+
     private String escape(String javaName) {
         StringBuffer result = new StringBuffer();
         int length = javaName.length();
         for(int i = 0; i < length; i++) {
             char c = javaName.charAt(i);
-            if (c == '$') {
-                result.append("_DOLLAR_");
+            if (c == '$' ) {
+                result.append(configuration.dollarReplacementInField());
             } else if (c == '_') {
-                result.append("__");
+                result.append(configuration.underscoreReplacementInField());
             } else {
                 result.append(c);
             }
