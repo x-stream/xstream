a6ac9265776d78cd7842e47035dcfbdc840eb747
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2006-04-13 19:49:41 +0000

    test for concurrent exception modification
    git-svn-id: http://svn.codehaus.org/xstream/trunk@814 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/core/DefaultConverterLookupTest.java
+++ xstream/src/test/com/thoughtworks/xstream/core/DefaultConverterLookupTest.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.xstream.core;
 
+import java.util.BitSet;
+
 import junit.framework.TestCase;
 
+import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.SingleValueConverterWrapper;
 import com.thoughtworks.xstream.converters.basic.StringConverter;
+import com.thoughtworks.xstream.converters.collections.BitSetConverter;
 import com.thoughtworks.xstream.mapper.DefaultMapper;
 
 /**
@@ -13,11 +17,16 @@ import com.thoughtworks.xstream.mapper.DefaultMapper;
 public class DefaultConverterLookupTest extends TestCase {
 
 	public void testConverterChange() {
+		
+		// this test actually depends on the keyset implementation of the corresponding cache map.
 		final DefaultConverterLookup lookup = new DefaultConverterLookup(
 				new DefaultMapper(Thread.currentThread()
 						.getContextClassLoader()));
 		Converter currentConverter = new SingleValueConverterWrapper(new StringConverter());
+		lookup.registerConverter(new BitSetConverter(), XStream.PRIORITY_VERY_HIGH);
 		lookup.registerConverter(currentConverter, -100);
+		lookup.lookupConverterForType(String.class);
+		lookup.lookupConverterForType(BitSet.class);
 		assertEquals(lookup.lookupConverterForType(String.class), currentConverter);
 		Converter newConverter = new SingleValueConverterWrapper(new StringConverter());
 		lookup.registerConverter(newConverter, 100);
