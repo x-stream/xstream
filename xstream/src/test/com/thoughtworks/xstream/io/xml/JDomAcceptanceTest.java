94d47c2ede22f59518bc18f00b973a99a25f5e4b
Jörg Schaible <joerg.schaible@gmx.de>
2006-04-28 06:44:19 +0000

    Fix format.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@846 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/io/xml/JDomAcceptanceTest.java
+++ xstream/src/test/com/thoughtworks/xstream/io/xml/JDomAcceptanceTest.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2004-2006 Elsag Solutions AG, Germany. All rights reserved.
- * www.elsag-solutions.com
- *
- * $Id$
- */
-
 package com.thoughtworks.xstream.io.xml;
 
 import java.io.StringReader;
@@ -21,87 +14,61 @@ import com.thoughtworks.acceptance.someobjects.X;
 import com.thoughtworks.acceptance.someobjects.Y;
 import com.thoughtworks.xstream.XStream;
 
+public class JDomAcceptanceTest extends TestCase {
 
-/**
- * DOCUMENT_ME
- *
- * @author  jos / last modified by $Author$
- * @version $Revision$
- */
-public class JDomAcceptanceTest extends TestCase
-{
-	//~ Instance fields --------------------------------------------------------
+    private XStream xstream;
 
-	private XStream xstream;
+    protected void setUp() throws Exception {
+        super.setUp();
+        xstream = new XStream();
+        xstream.alias("x", X.class);
+    }
 
-	//~ Methods ----------------------------------------------------------------
+    public void testUnmarshalsObjectFromJDOM() throws Exception {
+        String xml =
+                "<x>" +
+                "  <aStr>joe</aStr>" +
+                "  <anInt>8</anInt>" +
+                "  <innerObj>" +
+                "    <yField>walnes</yField>" +
+                "  </innerObj>" +
+                "</x>";
 
-	/**
-	 * DOCUMENT_ME
-	 *
-	 * @throws Exception DOCUMENT_ME
-	 */
-	protected void setUp() throws Exception
-	{
-		super.setUp();
-		xstream = new XStream();
-		xstream.alias("x", X.class);
-	}
+        Document doc = new SAXBuilder().build(new StringReader(xml));
 
-	/**
-	 * DOCUMENT_ME
-	 *
-	 * @throws Exception DOCUMENT_ME
-	 */
-	public void testUnmarshalsObjectFromJDOM() throws Exception
-	{
-		String xml =
-			"<x>"
-			+ "  <aStr>joe</aStr>"
-			+ "  <anInt>8</anInt>"
-			+ "  <innerObj>"
-			+ "    <yField>walnes</yField>"
-			+ "  </innerObj>"
-			+ "</x>";
+        X x = (X) xstream.unmarshal(new JDomReader(doc));
 
-		Document doc = new SAXBuilder().build(new StringReader(xml));
+        assertEquals("joe", x.aStr);
+        assertEquals(8, x.anInt);
+        assertEquals("walnes", x.innerObj.yField);
+    }
 
-		X x = (X) xstream.unmarshal(new JDomReader(doc));
+    public void testMarshalsObjectToJDOM() {
+        X x = new X();
+        x.anInt = 9;
+        x.aStr = "zzz";
+        x.innerObj = new Y();
+        x.innerObj.yField = "ooo";
 
-		assertEquals("joe", x.aStr);
-		assertEquals(8, x.anInt);
-		assertEquals("walnes", x.innerObj.yField);
-	}
+        String expected =
+                "<x>\n" +
+                "  <aStr>zzz</aStr>\n" +
+                "  <anInt>9</anInt>\n" +
+                "  <innerObj>\n" +
+                "    <yField>ooo</yField>\n" +
+                "  </innerObj>\n" +
+                "</x>";
 
-	/**
-	 * DOCUMENT_ME
-	 */
-	public void testMarshalsObjectToJDOM()
-	{
-		X x = new X();
-		x.anInt = 9;
-		x.aStr = "zzz";
-		x.innerObj = new Y();
-		x.innerObj.yField = "ooo";
+        JDomWriter writer = new JDomWriter();
+        xstream.marshal(x, writer);
+        List result = writer.getResult();
 
-		String expected =
-			"<x>\n"
-			+ "  <aStr>zzz</aStr>\n"
-			+ "  <anInt>9</anInt>\n"
-			+ "  <innerObj>\n"
-			+ "    <yField>ooo</yField>\n"
-			+ "  </innerObj>\n"
-			+ "</x>";
+        assertEquals("Result list should contain exactly 1 element",
+                                                        1, result.size());
 
-		JDomWriter writer = new JDomWriter();
-		xstream.marshal(x, writer);
+        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n"));
 
-		List result = writer.getResult();
-
-		assertEquals("Result list should contain exactly 1 element", 1, result.size());
-
-		XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n"));
-
-		assertEquals(expected, outputter.outputString(result));
-	}
+        assertEquals(expected, outputter.outputString(result));
+    }
 }
+
