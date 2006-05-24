2c9aafcde1b4870a809a6b8903a9bdf41ebf320b
Jörg Schaible <joerg.schaible@gmx.de>
2006-05-24 16:08:00 +0000

    Fix strange formatting.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@853 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/core/util/PrioritizedList.java
+++ xstream/src/java/com/thoughtworks/xstream/core/util/PrioritizedList.java
@@ -4,29 +4,28 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
+
 /**
  * List that allows items to be added with a priority that will affect the order in which they are later iterated over.
- *
  * Objects with a high priority will appear before objects with a low priority in the list. If two objects of the same
- * priority are added to the list, the most recently added one will be iterated over first.
- *
- * Implementation uses a TreeSet, which has a garanteed add time of O(log(n)).
+ * priority are added to the list, the most recently added one will be iterated over first. Implementation uses a
+ * TreeSet, which has a garanteed add time of O(log(n)).
  * 
  * @author Joe Walnes
  * @author Guilherme Silveira
  */
 public class PrioritizedList {
-	
+
     private final Set set = new TreeSet();
 
     private int lowestPriority = Integer.MAX_VALUE;
-    
+
     private int lastId = 0;
 
     public void add(Object item, int priority) {
-    	if (this.lowestPriority > priority) {
-			this.lowestPriority = priority;
-		}
+        if (this.lowestPriority > priority) {
+            this.lowestPriority = priority;
+        }
         this.set.add(new PrioritizedItem(item, priority, ++lastId));
     }
 
@@ -38,7 +37,7 @@ public class PrioritizedList {
 
         final Object value;
         final int priority;
-		final int id;
+        final int id;
 
         public PrioritizedItem(Object value, int Prioritized, int id) {
             this.value = value;
@@ -46,23 +45,23 @@ public class PrioritizedList {
             this.id = id;
         }
 
-		public int compareTo(Object o) {
-			PrioritizedItem other = (PrioritizedItem) o;
-			if (this.priority != other.priority) {
-				return (other.priority - this.priority);
-			}
-			return (other.id - this.id);
-		}
-		
-		public boolean equals(Object obj) {
-			return this.id == ((PrioritizedItem) obj).id; 
-		}
+        public int compareTo(Object o) {
+            PrioritizedItem other = (PrioritizedItem)o;
+            if (this.priority != other.priority) {
+                return (other.priority - this.priority);
+            }
+            return (other.id - this.id);
+        }
+
+        public boolean equals(Object obj) {
+            return this.id == ((PrioritizedItem)obj).id;
+        }
 
     }
 
     private static class PrioritizedItemIterator implements Iterator {
 
-	private Iterator iterator;
+        private Iterator iterator;
 
         public PrioritizedItemIterator(Iterator iterator) {
             this.iterator = iterator;
