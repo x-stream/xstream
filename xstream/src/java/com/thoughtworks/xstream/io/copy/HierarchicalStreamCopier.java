c512be001ba5a988bdee6c0ec316376a9cfbb9a9
Joe Walnes <joe@truemesh.com>
2006-06-04 18:52:14 +0000

    Added HierStreamCopier that allows one format to be copied to another without the overhead of serialization.
    This can be used for transforming documents serialized in XML to binary and vice-versa.
    
    git-svn-id: http://svn.codehaus.org/xstream/trunk@861 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/java/com/thoughtworks/xstream/io/copy/HierarchicalStreamCopier.java
@@ -0,0 +1,40 @@
+package com.thoughtworks.xstream.io.copy;
+
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+/**
+ * Tool for copying the contents of one HierarichalStreamReader to a HierarichalStreamWriter.
+ * <p/>
+ * This is useful for transforming the output of one format to another (e.g. binary to XML)
+ * without needing to know details about the classes and avoiding the overhead of serialization.
+ *
+ * <h3>Example</h3>
+ * <pre>
+ * HierarchicalStreamReader reader = new BinaryReader(someBinaryInput);
+ * HierarchicalStreamWriter writer = new PrettyPrintWriter(someXmlOutput);
+ * HierarchicalStreamCopier copier = new HierarchicalStreamCopier();
+ * copier.copy(reader, writer);
+ * </pre>
+ *
+ * @author Joe Walnes
+ */
+public class HierarchicalStreamCopier {
+    public void copy(HierarchicalStreamReader source, HierarchicalStreamWriter destination) {
+        destination.startNode(source.getNodeName());
+        int attributeCount = source.getAttributeCount();
+        for (int i = 0; i < attributeCount; i++) {
+            destination.addAttribute(source.getAttributeName(i), source.getAttribute(i));
+        }
+        String value = source.getValue();
+        if (value != null && value.length() > 0) {
+            destination.setValue(value);
+        }
+        while (source.hasMoreChildren()) {
+            source.moveDown();
+            copy(source, destination);
+            source.moveUp();
+        }
+        destination.endNode();
+    }
+}
