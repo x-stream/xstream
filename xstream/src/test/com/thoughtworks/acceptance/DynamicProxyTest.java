3f008daeaf27d966936b967a7e7076ed5b6fe394
Jörg Schaible <joerg.schaible@gmx.de>
2006-10-11 14:54:48 +0000

    Add test case for proxy bug.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@969 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/acceptance/DynamicProxyTest.java
+++ xstream/src/test/com/thoughtworks/acceptance/DynamicProxyTest.java
@@ -2,17 +2,40 @@ package com.thoughtworks.acceptance;
 
 import com.thoughtworks.acceptance.objects.SampleDynamicProxy;
 
+
 public class DynamicProxyTest extends AbstractAcceptanceTest {
+    public static class ClassWithProxyMember {
+        SampleDynamicProxy.InterfaceOne one;
+        SampleDynamicProxy.InterfaceTwo two;
+    };
 
     public void testDynamicProxy() {
-        assertBothWays(SampleDynamicProxy.newInstance(),
-                "<dynamic-proxy>\n" +
-                "  <interface>com.thoughtworks.acceptance.objects.SampleDynamicProxy$InterfaceOne</interface>\n" +
-                "  <interface>com.thoughtworks.acceptance.objects.SampleDynamicProxy$InterfaceTwo</interface>\n" +
-                "  <handler class=\"com.thoughtworks.acceptance.objects.SampleDynamicProxy\">\n" +
-                "    <aField>hello</aField>\n" +
-                "  </handler>\n" +
-                "</dynamic-proxy>");
+        assertBothWays(SampleDynamicProxy.newInstance(), ""
+                + "<dynamic-proxy>\n"
+                + "  <interface>com.thoughtworks.acceptance.objects.SampleDynamicProxy$InterfaceOne</interface>\n"
+                + "  <interface>com.thoughtworks.acceptance.objects.SampleDynamicProxy$InterfaceTwo</interface>\n"
+                + "  <handler class=\"com.thoughtworks.acceptance.objects.SampleDynamicProxy\">\n"
+                + "    <aField>hello</aField>\n"
+                + "  </handler>\n"
+                + "</dynamic-proxy>");
+    }
+
+    public void TODOtestDynamicProxyAsFieldMember() {
+        ClassWithProxyMember expected = new ClassWithProxyMember();
+        expected.one = (SampleDynamicProxy.InterfaceOne)SampleDynamicProxy.newInstance();
+        expected.two = (SampleDynamicProxy.InterfaceTwo)expected.one;
+        xstream.alias("with-proxy", ClassWithProxyMember.class);
+        assertBothWays(expected, ""
+                + "<with-proxy>\n"
+                + "  <one class=\"dynamic-proxy\">\n"
+                + "    <interface>com.thoughtworks.acceptance.objects.SampleDynamicProxy$InterfaceOne</interface>\n"
+                + "    <interface>com.thoughtworks.acceptance.objects.SampleDynamicProxy$InterfaceTwo</interface>\n"
+                + "    <handler class=\"com.thoughtworks.acceptance.objects.SampleDynamicProxy\">\n"
+                + "      <aField>hello</aField>\n"
+                + "    </handler>\n"
+                + "  </one>\n"
+                + "  <two class=\"dynamic-proxy\" reference=\"../one\"/>\n"
+                + "</with-proxy>");
     }
 
 }
