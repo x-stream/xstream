78b327bdc02169642767891b8a6b2f4497a3c01c
Jörg Schaible <joerg.schaible@gmx.de>
2006-10-18 21:00:43 +0000

    Fix javadoc.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@993 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/io/xml/DocumentReader.java
+++ xstream/src/java/com/thoughtworks/xstream/io/xml/DocumentReader.java
@@ -1,10 +1,6 @@
 package com.thoughtworks.xstream.io.xml;
 
-import java.util.Iterator;
-import java.util.List;
-
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
 
 /**
@@ -16,13 +12,9 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 public interface DocumentReader extends HierarchicalStreamReader {
 
     /**
-     * Retrieve the current element. In the standrad use case this list will only contain a single element.
-     * Additional elements can only occur, if {@link HierarchicalStreamWriter#startNode(String)} of the implementing
-     * {@link HierarchicalStreamWriter} was called multiple times with an empty node stack. Such a situation occurs
-     * calling {@link com.thoughtworks.xstream.XStream#marshal(Object, HierarchicalStreamWriter)} multiple times
-     * directly.
+     * Retrieve the current processed node of the DOM.
      * 
-     * @return a {@link List} with top nodes
+     * @return the current node
      * @since upcoming
      */
     public Object getCurrent();
