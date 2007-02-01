ec1dc4d2ae9a4caabcc564d9edc332c6c317b967
Jörg Schaible <joerg.schaible@gmx.de>
2007-02-01 20:38:24 +0000

    Implement a more generic TextAttributeConverter not depending on JDK version (STR-378).
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1097 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/extended/TextAttributeConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/TextAttributeConverter.java
@@ -1,14 +1,9 @@
 package com.thoughtworks.xstream.converters.extended;
 
-import com.thoughtworks.xstream.converters.basic.AbstractSingleValueConverter;
-import com.thoughtworks.xstream.converters.reflection.ObjectAccessException;
+import com.thoughtworks.xstream.converters.reflection.AbstractAttributedCharacterIteratorAttributeConverter;
 
 import java.awt.font.TextAttribute;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.text.AttributedCharacterIterator;
-import java.util.Map;
+
 
 /**
  * A converter for {@link TextAttribute} constants.
@@ -16,46 +11,15 @@ import java.util.Map;
  * @author J&ouml;rg Schaible
  * @since 1.2
  */
-public class TextAttributeConverter  extends AbstractSingleValueConverter {
-    
-    private static final Method getName;
-    private static final Field instanceMap;
-    static {
-        try {
-            getName = AttributedCharacterIterator.Attribute.class.getDeclaredMethod("getName", (Class[])null);
-            instanceMap = TextAttribute.class.getDeclaredField("instanceMap");
-        } catch (NoSuchMethodException e) {
-            throw new InternalError("Missing TextAttribute.getName()");
-        } catch (NoSuchFieldException e) {
-            throw new InternalError("Missing TextAttribute.instanceMap");
-        }
-    }
+public class TextAttributeConverter extends
+    AbstractAttributedCharacterIteratorAttributeConverter {
 
-    public boolean canConvert(Class type) {
-        return type == TextAttribute.class;
+    /**
+     * Constructs a TextAttributeConverter.
+     * 
+     * @since upcoming
+     */
+    public TextAttributeConverter() {
+        super(TextAttribute.class);
     }
-
-    public String toString(Object source) {
-        TextAttribute attribute = (TextAttribute)source;
-        try {
-            getName.setAccessible(true);
-            return (String)getName.invoke(attribute, (Object[])null);
-        } catch (IllegalAccessException e) {
-            throw new ObjectAccessException("Cannot get name of TextAttribute", e);
-        } catch (InvocationTargetException e) {
-            throw new ObjectAccessException("Cannot get name of TextAttribute", e.getTargetException());
-        }
-    }
-
-    public Object fromString(String str) {
-        instanceMap.setAccessible(true);
-        Map map;
-        try {
-            map = (Map)instanceMap.get(null);
-        } catch (IllegalAccessException e) {
-            throw new ObjectAccessException("Cannot get named instance of TextAttribute " + str);
-        }
-        return map.get(str);
-    }
-
 }
---
+++ xstream/src/java/com/thoughtworks/xstream/converters/reflection/AbstractAttributedCharacterIteratorAttributeConverter.java
@@ -0,0 +1,88 @@
+package com.thoughtworks.xstream.converters.reflection;
+
+import com.thoughtworks.xstream.converters.basic.AbstractSingleValueConverter;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.text.AttributedCharacterIterator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+/**
+ * An abstract converter implementation for constants of
+ * {@link AttributedCharacterIterator.Attribute} and derived types.
+ * 
+ * @author J&ouml;rg Schaible
+ * @since upcoming
+ */
+public class AbstractAttributedCharacterIteratorAttributeConverter extends
+    AbstractSingleValueConverter {
+
+    private static final Method getName;
+    static {
+        try {
+            getName = AttributedCharacterIterator.Attribute.class.getDeclaredMethod(
+                "getName", (Class[])null);
+        } catch (NoSuchMethodException e) {
+            throw new InternalError("Missing AttributedCharacterIterator.Attribute.getName()");
+        }
+    }
+
+    private final Class type;
+    private transient Map attributeMap;
+    private transient FieldDictionary fieldDictionary;
+
+    public AbstractAttributedCharacterIteratorAttributeConverter(final Class type) {
+        super();
+        this.type = type;
+        readResolve();
+    }
+
+    public boolean canConvert(final Class type) {
+        return type == this.type;
+    }
+
+    public String toString(final Object source) {
+        AttributedCharacterIterator.Attribute attribute = (AttributedCharacterIterator.Attribute)source;
+        try {
+            if (!getName.isAccessible()) {
+                getName.setAccessible(true);
+            }
+            return (String)getName.invoke(attribute, (Object[])null);
+        } catch (IllegalAccessException e) {
+            throw new ObjectAccessException(
+                "Cannot get name of AttributedCharacterIterator.Attribute", e);
+        } catch (InvocationTargetException e) {
+            throw new ObjectAccessException(
+                "Cannot get name of AttributedCharacterIterator.Attribute", e
+                    .getTargetException());
+        }
+    }
+
+    public Object fromString(final String str) {
+        return attributeMap.get(str);
+    }
+
+    private Object readResolve() {
+        fieldDictionary = new FieldDictionary();
+        attributeMap = new HashMap();
+        for (final Iterator iterator = fieldDictionary.serializableFieldsFor(type); iterator
+            .hasNext();) {
+            final Field field = (Field)iterator.next();
+            if (field.getType() == type && Modifier.isStatic(field.getModifiers())) {
+                try {
+                    final Object attribute = field.get(null);
+                    attributeMap.put(toString(attribute), attribute);
+                } catch (IllegalAccessException e) {
+                    throw new ObjectAccessException("Cannot get object of " + field, e);
+                }
+            }
+        }
+        return this;
+    }
+
+}
