95f613aeb072c9df3ebeba23a561bce62abfc0cb
Jörg Schaible <joerg.schaible@gmx.de>
2007-02-01 20:35:11 +0000

    Modified synchronization.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1096 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/reflection/FieldDictionary.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/reflection/FieldDictionary.java
@@ -4,23 +4,23 @@ import com.thoughtworks.xstream.core.JVM;
 import com.thoughtworks.xstream.core.util.OrderRetainingMap;
 
 import java.lang.reflect.Field;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+
 /**
- * A field dictionary instance caches information about classes fields. 
- *
+ * A field dictionary instance caches information about classes fields.
  */
 public class FieldDictionary {
 
-    private final Map keyedByFieldNameCache = Collections.synchronizedMap(new HashMap());
-    private final Map keyedByFieldKeyCache = Collections.synchronizedMap(new HashMap());
+    private final Map keyedByFieldNameCache = new HashMap();
+    private final Map keyedByFieldKeyCache = new HashMap();
 
     /**
      * Returns an iterator for all serializable fields for some class
-     * @param cls	the class you are interested on
+     * 
+     * @param cls the class you are interested on
      * @return an iterator for its serializable fields
      */
     public Iterator serializableFieldsFor(Class cls) {
@@ -28,17 +28,20 @@ public class FieldDictionary {
     }
 
     /**
-     * Returns an specific field of some class. If definedIn is null, it searchs for the field named 'name' inside the class cls.
-     * If definedIn is different than null, tries to find the specified field name in the specified class cls which should be defined in
-     * class definedIn (either equals cls or a one of it's superclasses)  
-     * @param cls	the class where the field is to be searched
-     * @param name	the field name
-     * @param definedIn	the superclass (or the class itself) of cls where the field was defined
+     * Returns an specific field of some class. If definedIn is null, it searchs for the field
+     * named 'name' inside the class cls. If definedIn is different than null, tries to find the
+     * specified field name in the specified class cls which should be defined in class
+     * definedIn (either equals cls or a one of it's superclasses)
+     * 
+     * @param cls the class where the field is to be searched
+     * @param name the field name
+     * @param definedIn the superclass (or the class itself) of cls where the field was defined
      * @return the field itself
      */
     public Field field(Class cls, String name, Class definedIn) {
         Map fields = buildMap(cls, definedIn != null);
-        Field field = (Field) fields.get(definedIn != null ? (Object) new FieldKey(name, definedIn, 0) : (Object) name);
+        Field field = (Field)fields.get(definedIn != null ? (Object)new FieldKey(
+            name, definedIn, 0) : (Object)name);
         if (field == null) {
             throw new ObjectAccessException("No such field " + cls.getName() + "." + name);
         } else {
@@ -48,16 +51,16 @@ public class FieldDictionary {
 
     private Map buildMap(Class cls, boolean tupleKeyed) {
         final String clsName = cls.getName();
-        if (!keyedByFieldNameCache.containsKey(clsName)) {
+        synchronized (keyedByFieldNameCache) {
             synchronized (keyedByFieldKeyCache) {
-                if (!keyedByFieldNameCache.containsKey(clsName)) { // double check
+                if (!keyedByFieldNameCache.containsKey(clsName)) {
                     final Map keyedByFieldName = new HashMap();
                     final Map keyedByFieldKey = new OrderRetainingMap();
                     while (!Object.class.equals(cls)) {
                         Field[] fields = cls.getDeclaredFields();
                         if (JVM.reverseFieldDefinition()) {
                             for (int i = fields.length >> 1; i-- > 0;) {
-                                final int idx = fields.length-i-1;
+                                final int idx = fields.length - i - 1;
                                 final Field field = fields[i];
                                 fields[i] = fields[idx];
                                 fields[idx] = field;
@@ -69,7 +72,8 @@ public class FieldDictionary {
                             if (!keyedByFieldName.containsKey(field.getName())) {
                                 keyedByFieldName.put(field.getName(), field);
                             }
-                            keyedByFieldKey.put(new FieldKey(field.getName(), field.getDeclaringClass(), i), field);
+                            keyedByFieldKey.put(new FieldKey(field.getName(), field
+                                .getDeclaringClass(), i), field);
                         }
                         cls = cls.getSuperclass();
                     }
@@ -78,7 +82,8 @@ public class FieldDictionary {
                 }
             }
         }
-        return (Map) (tupleKeyed ? keyedByFieldKeyCache.get(clsName) : keyedByFieldNameCache.get(clsName));
+        return (Map)(tupleKeyed ? keyedByFieldKeyCache.get(clsName) : keyedByFieldNameCache
+            .get(clsName));
     }
 
     private static class FieldKey {
@@ -104,10 +109,14 @@ public class FieldDictionary {
             if (this == o) return true;
             if (!(o instanceof FieldKey)) return false;
 
-            final FieldKey fieldKey = (FieldKey) o;
+            final FieldKey fieldKey = (FieldKey)o;
 
-            if (declaringClass != null ? !declaringClass.equals(fieldKey.declaringClass) : fieldKey.declaringClass != null) return false;
-            if (fieldName != null ? !fieldName.equals(fieldKey.fieldName) : fieldKey.fieldName != null) return false;
+            if (declaringClass != null
+                ? !declaringClass.equals(fieldKey.declaringClass)
+                : fieldKey.declaringClass != null) return false;
+            if (fieldName != null
+                ? !fieldName.equals(fieldKey.fieldName)
+                : fieldKey.fieldName != null) return false;
 
             return true;
         }
@@ -120,14 +129,18 @@ public class FieldDictionary {
         }
 
         public String toString() {
-            return "FieldKey{" +
-                    "order=" + order +
-                    ", writer=" + depth +
-                    ", declaringClass=" + declaringClass +
-                    ", fieldName='" + fieldName + "'" +
-                    "}";
+            return "FieldKey{"
+                + "order="
+                + order
+                + ", writer="
+                + depth
+                + ", declaringClass="
+                + declaringClass
+                + ", fieldName='"
+                + fieldName
+                + "'"
+                + "}";
         }
-
 
     }
 
