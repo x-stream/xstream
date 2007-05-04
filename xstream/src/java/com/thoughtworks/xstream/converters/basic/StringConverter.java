78f7e0932597380eb5d5624e0d1d079b6a0def38
Jörg Schaible <joerg.schaible@gmx.de>
2007-05-04 21:25:27 +0000

    Avoid String.intern() wasting limited PermGen space (XSTR-395).)
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1151 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/basic/StringConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/basic/StringConverter.java
@@ -1,22 +1,50 @@
 package com.thoughtworks.xstream.converters.basic;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 /**
  * Converts a String to a String ;). Well ok, it doesn't
  * <i>actually</i> do any conversion.
- * <p>The converter always calls intern() on the returned
- * String to encourage the JVM to reuse instances.</p>
+ * <p>The converter uses a map to reuse instances. This map is by default a synchronized {@link WeakHashMap}.</p>
  *
  * @author Joe Walnes
+ * @author Rene Schwietzke
  * @see String#intern()
  */
 public class StringConverter extends AbstractSingleValueConverter {
+
+    /**
+     * A Map to store strings as long as needed to map similar strings onto the same
+     * instance and conserve memory. The map can be set from the outside during
+     * construction, so it can be a lru map or a weak map, sychronised or not.
+     */
+    private final Map cache;
+
+    public StringConverter(Map map) {
+        this.cache = map;
+    }
+
+    public StringConverter() {
+        this(Collections.synchronizedMap(new WeakHashMap()));
+    }
 
     public boolean canConvert(Class type) {
         return type.equals(String.class);
     }
 
     public Object fromString(String str) {
-        return str.intern();
+        String s = (String)cache.get(str);
+
+        if (s == null) {
+            // fill cache
+            cache.put(str, str);
+
+            s = str;
+        }
+
+        return s;
     }
 
 }
