f95e7b4550d6dd4a3cee2764992ef4d99f6e9bfe
Jörg Schaible <joerg.schaible@gmx.de>
2007-06-18 08:47:30 +0000

    Fix cache in StringConverter (XSTR-410).
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1194 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/basic/StringConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/basic/StringConverter.java
@@ -1,50 +1,54 @@
 package com.thoughtworks.xstream.converters.basic;
 
+import java.lang.ref.WeakReference;
 import java.util.Collections;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+
 /**
- * Converts a String to a String ;). Well ok, it doesn't
- * <i>actually</i> do any conversion.
- * <p>The converter uses a map to reuse instances. This map is by default a synchronized {@link WeakHashMap}.</p>
+ * Converts a String to a String ;).
+ * <p>
+ * Well ok, it doesn't <i>actually</i> do any conversion. The converter uses a map to reuse instances. This map is by
+ * default a synchronized {@link WeakHashMap}.
+ * </p>
  *
  * @author Joe Walnes
  * @author Rene Schwietzke
+ * @author J&ouml;rg Schaible
  * @see String#intern()
  */
 public class StringConverter extends AbstractSingleValueConverter {
 
     /**
-     * A Map to store strings as long as needed to map similar strings onto the same
-     * instance and conserve memory. The map can be set from the outside during
-     * construction, so it can be a lru map or a weak map, sychronised or not.
+     * A Map to store strings as long as needed to map similar strings onto the same instance and conserve memory. The
+     * map can be set from the outside during construction, so it can be a LRU map or a weak map, sychronised or not.
      */
     private final Map cache;
 
-    public StringConverter(Map map) {
-        this.cache = map;
+    public StringConverter(final Map map) {
+        cache = map;
     }
 
     public StringConverter() {
         this(Collections.synchronizedMap(new WeakHashMap()));
     }
 
-    public boolean canConvert(Class type) {
+    public boolean canConvert(final Class type) {
         return type.equals(String.class);
     }
 
-    public Object fromString(String str) {
-        String s = (String)cache.get(str);
+    public Object fromString(final String str) {
+        final WeakReference ref = (WeakReference)cache.get(str);
+        String s = (String)(ref == null ? null : ref.get());
 
         if (s == null) {
             // fill cache
-            cache.put(str, str);
+            cache.put(str, new WeakReference(str));
 
             s = str;
         }
 
         return s;
     }
-
 }
