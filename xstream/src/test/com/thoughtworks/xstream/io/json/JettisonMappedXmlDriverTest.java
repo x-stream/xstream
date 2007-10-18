48bc7be5914055c7cb836e268eca929911b2e15a
Jörg Schaible <joerg.schaible@gmx.de>
2007-10-18 06:58:27 +0000

    Unify Jettison-based unit tests.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1273 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/io/json/JettisonMappedXmlDriverTest.java
+++ xstream/src/test/com/thoughtworks/xstream/io/json/JettisonMappedXmlDriverTest.java
@@ -17,13 +17,21 @@ import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
  */
 public class JettisonMappedXmlDriverTest extends TestCase {
 
-    private String simpleJson = "{\"com.thoughtworks.acceptance.objects.Product\":{\"name\":\"Banana\",\"id\":\"123\",\"price\":\"23.0\"}}";
+    private String simpleJson = "{\"product\":{\"name\":\"Banana\",\"id\":\"123\",\"price\":\"23.0\"}}";
     private String hiearchyJson = "{\"category\":{\"name\":\"fruit\",\"id\":\"111\",\"products\":{\"product\":[{\"name\":\"Banana\",\"id\":\"123\",\"price\":\"23.0\",\"tags\":{\"string\":[\"yellow\",\"fresh\",\"tasty\"]}},{\"name\":\"Mango\",\"id\":\"124\",\"price\":\"34.0\"}]}}}";
+    private XStream xstream;
 
-    public void testReadSimple() {
-        XStream xstream = new XStream(new JettisonMappedXmlDriver());
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        xstream = new XStream(new JettisonMappedXmlDriver());
         xstream.alias("category", Category.class);
         xstream.alias("product", Product.class);
+    }
+
+    public void testReadSimple() {
         Product product = (Product)xstream.fromXML(simpleJson);
         assertEquals(product.getName(), "Banana");
         assertEquals(product.getId(), "123");
@@ -32,10 +40,7 @@ public class JettisonMappedXmlDriverTest extends TestCase {
 
     public void testWriteSimple() {
         Product product = new Product("Banana", "123", 23.00);
-        XStream xstream = new XStream(new JettisonMappedXmlDriver());
-
         String result = xstream.toXML(product);
-
         assertEquals(result, simpleJson);
     }
 
@@ -52,20 +57,13 @@ public class JettisonMappedXmlDriverTest extends TestCase {
         Product mango = new Product("Mango", "124", 34.00);
         products.add(mango);
         category.setProducts(products);
-        XStream xstream = new XStream(new JettisonMappedXmlDriver());
-        xstream.alias("category", Category.class);
-        xstream.alias("product", Product.class);
         String result = xstream.toXML(category);
         assertEquals(result, hiearchyJson);
     }
 
     public void testHierarchyRead() {
-        XStream xstream = new XStream(new JettisonMappedXmlDriver());
-        xstream.alias("category", Category.class);
-        xstream.alias("product", Product.class);
         Category parsedCategory = (Category)xstream.fromXML(hiearchyJson);
         Product parsedBanana = (Product)parsedCategory.getProducts().get(0);
-
         assertEquals(parsedBanana.getName(), "Banana");
         assertEquals(parsedBanana.getTags().size(), 3);
         assertEquals(parsedBanana.getTags().get(0), "yellow");
