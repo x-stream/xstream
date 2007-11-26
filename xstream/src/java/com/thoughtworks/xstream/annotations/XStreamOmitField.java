3769a44cd13920449ba74ebfc5fa8c4045eec781
Jörg Schaible <joerg.schaible@gmx.de>
2007-11-26 00:41:54 +0000

    Spelling.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1329 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/annotations/XStreamOmitField.java
+++ xstream/src/java/com/thoughtworks/xstream/annotations/XStreamOmitField.java
@@ -6,11 +6,12 @@ import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
 /**
- * Declares a field as to be ommited. The result is the same as invoking the method
- * omitField in a xstream instance.
+ * Declares a field to be omitted. The result is the same as invoking the method
+ * omitField in a XStream instance.
  * 
  * @author Chung-Onn Cheong
  * @author Guilherme Silveira
+ * @since 1.2.2
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
