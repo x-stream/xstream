a29d9b4b3f0cd1bf4670737a0cce842290d7150b
Jörg Schaible <joerg.schaible@gmx.de>
2008-02-11 23:11:49 +0000

    Fix license header.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1409 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/converters/enums/Fruit.java
+++ xstream/src/test/com/thoughtworks/xstream/converters/enums/Fruit.java
@@ -2,6 +2,10 @@
  * Copyright (C) 2008 XStream Committers.
  * All rights reserved.
  *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ *
  * Created on 11.02.2008 by Joerg Schaible
  */
 package com.thoughtworks.xstream.converters.enums;
