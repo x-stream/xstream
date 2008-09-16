9abaa5e3b9f7f98cbcce72b6ad3c200e02ffa586
Jörg Schaible <joerg.schaible@gmx.de>
2008-09-16 07:55:19 +0000

    Different approach to test ObjectIdMap based on hash value clashes.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1514 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/core/util/ObjectIdDictionaryTest.java
+++ xstream/src/test/com/thoughtworks/xstream/core/util/ObjectIdDictionaryTest.java
@@ -14,7 +14,9 @@ package com.thoughtworks.xstream.core.util;
 import junit.framework.TestCase;
 
 import java.lang.ref.SoftReference;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -45,48 +47,48 @@ public class ObjectIdDictionaryTest extends TestCase {
         assertEquals("id b", dict.lookupId(b));
     }
 
-    public void testEnforceSameSystemHashCodeForGCedObjects() {
+    public void testEnforceSameSystemHashCodeForGCedObjects() throws NoSuchFieldException, IllegalAccessException {
+        System.setProperty("xstream.debug", "true");
+
+        final Field invalidCounter = ObjectIdDictionary.class.getDeclaredField("invalidCounter");
+        invalidCounter.setAccessible(true);
+        ObjectIdDictionary dict = new ObjectIdDictionary();
+        invalidCounter.setInt(dict, 1);
+        
         final StringBuffer memInfo = new StringBuffer("JVM: ");
         memInfo.append(System.getProperty("java.version"));
         memInfo.append("\nMemoryInfo:\n");
-        System.setProperty("xstream.debug", "true");
-
-        try {
-        int blocks = forceGCAndGetNumberOfBlocks(memInfo)/5;
-        List softMemory = new ArrayList();
-        while (blocks-- > 0) {
-            softMemory.add(blocks < 60 ? (Object)new SoftReference(new byte[1024*80]) : (Object)new byte[1024*80]);
-        }
-        forceGCAndGetNumberOfBlocks(memInfo);
+        memInfo.append(memoryInfo());
+        memInfo.append('\n');
         
-        // create 200000 Strings and call GC after creation of 50000
-        final int loop = 4;
-        final int elements = 50000;
-        final int[] dictSizes = new int[loop * elements];
-        ObjectIdDictionary dict = new ObjectIdDictionary();
-        final Set systemHashes = new HashSet();
-        for (int i = 0; i < loop; ++i) {
-            for (int j = 0; j < elements; ++j) {
-                final String s = new String("JUnit ") + j; // enforce new object
-                systemHashes.add(new Integer(System.identityHashCode(s)));
-                dictSizes[i * elements + j] = dict.size();
-                assertFalse("Failed in (" + i + "/" + j + ")", dict.containsId(s));
+        byte[] memReserve = new byte[1024 * 512];
+        Arrays.fill(memReserve, (byte)255); // prevent JVM optimisation
+        int oome = 0;
+        int counter = 0;
+        for(; invalidCounter.getInt(dict) != 0; ++counter) {
+            try {
+                final String s = new String("JUnit ") + counter; // enforce new object
+                assertFalse("Failed in (" + counter + ")", dict.containsId(s));
                 dict.associateId(s, "X");
+                if (counter % 10000 == 9999) {
+                    forceGC(memInfo);
+                }
+            } catch(OutOfMemoryError e) {
+                memReserve = null;
+                forceGC(memInfo);
+                if(++oome == 5) {
+                    System.out.println(memInfo);
+                    throw e;
+                }
+                memReserve = new byte[1024 * 512];
             }
-            forceGCAndGetNumberOfBlocks(memInfo);
         }
 
         System.setProperty("xstream.debug", "false");
-        assertFalse("Algorithm did not reach last element", 0 == dictSizes[loop * elements - 1]);
-        assertFalse("Dictionary did not shrink; " + systemHashes.size() + " distinct objects; " + memInfo,
-            loop * elements - 1 == dictSizes[loop * elements - 1]);
-        } catch(OutOfMemoryError e) {
-            System.out.println(memInfo);
-            throw e;
-        }
+        assertTrue("Dictionary did not shrink; " + counter + " distinct objects; " + dict.size() + " size; " + memInfo, dict.size() < 10);
     }
 
-    private int forceGCAndGetNumberOfBlocks(StringBuffer memInfo) {
+    private void forceGC(StringBuffer memInfo) {
         memInfo.append(memoryInfo());
         memInfo.append('\n');
 
@@ -106,18 +108,12 @@ public class ObjectIdDictionaryTest extends TestCase {
             memory.clear();
             memory = null;
             System.gc();
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                // ignore
-            }
         }
 
-        memInfo.append("Force GC, blocks: " + i);
+        memInfo.append("Force GC, allocated blocks of 16KB: " + i);
         memInfo.append('\n');
         
         assertNull("This JVM is not releasing memory!", ref.get());
-        return i;
     }
     
     private String memoryInfo() {
