ef5dd37458ff87f554ff525471c14f7b662a1670
Jörg Schaible <joerg.schaible@gmx.de>
2008-09-22 09:14:30 +0000

    Fix copyright year.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1535 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/io/xml/AbstractXmlReader.java
+++ xstream/src/java/com/thoughtworks/xstream/io/xml/AbstractXmlReader.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 Joe Walnes.
- * Copyright (C) 2006, 2007 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
