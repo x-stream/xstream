848f9a3097b12be60ec5e9a699236843ed0da404
Jörg Schaible <joerg.schaible@gmx.de>
2008-10-21 18:04:13 +0000

    Apply performance patch for SerializationMethodInvoker (XSTR-517).
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1566 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/reflection/SerializationMethodInvoker.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/reflection/SerializationMethodInvoker.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2004, 2005 Joe Walnes.
- * Copyright (C) 2006, 2007 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -104,11 +104,22 @@ public class SerializationMethodInvoker {
         }
     }
 
-    private Method getMethod(Class type, String name, Class[] parameterTypes, boolean includeBaseclasses) {
-        Object key = type.getName() + "." + name + "." + includeBaseclasses;
-        if (cache.containsKey(key)) {
-            Object result = cache.get(key);
-            return (Method) (result == NO_METHOD ? null : result);
+    private Method getMethod(Class type, String name, Class[] parameterTypes,
+        boolean includeBaseclasses) {
+        final String typeName = type.getName();
+        StringBuffer sb = new StringBuffer(typeName.length() + name.length() + 7);
+        sb
+            .append(typeName)
+            .append('.')
+            .append(name)
+            .append('.')
+            .append(includeBaseclasses)
+            .toString();
+        String key = sb.toString();
+        Object resultOb = cache.get(key);
+
+        if (resultOb != null) {
+            return (resultOb == NO_METHOD) ? null : ((Method)resultOb);
         }
         if (includeBaseclasses) {
             while (type != null) {
