55e1fe7098aa2fc64df4182d61e1c462544b9e74
Jörg Schaible <joerg.schaible@gmx.de>
2009-04-09 01:45:37 +0000

    Fix deprecation annotation (XSTR-561).
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1660 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/mapper/EnumMapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/EnumMapper.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2005 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -39,13 +39,13 @@ public class EnumMapper extends MapperWrapper {
     /**
      * @deprecated since 1.3.1, use {@link #EnumMapper(Mapper)}
      */
+    @Deprecated
     public EnumMapper(Mapper wrapped, ConverterLookup lookup) {
         super(wrapped);
         this.converterLookup = lookup;
         readResolve();
     }
 
-    @Deprecated
     public EnumMapper(Mapper wrapped) {
         super(wrapped);
         this.converterLookup = null;
