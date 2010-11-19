05cd4cbd20bb524587acc228a7326ce740906ab8
Jörg Schaible <joerg.schaible@gmx.de>
2010-11-19 14:23:39 +0000

    Make JDK 1.4 compatible.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1781 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/core/util/CloneablesTest.java
+++ xstream/src/test/com/thoughtworks/xstream/core/util/CloneablesTest.java
@@ -67,7 +67,6 @@ public class CloneablesTest extends TestCase {
             super(type);
         }
 
-        @Override
         public CloneableTypedNull clone() throws CloneNotSupportedException {
             return (CloneableTypedNull)super.clone();
         }
