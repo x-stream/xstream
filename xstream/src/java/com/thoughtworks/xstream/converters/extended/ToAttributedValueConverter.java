dafb8f840d16ba96269de265797f8d175dae42a4
Jörg Schaible <joerg.schaible@gmx.de>
2011-07-30 17:33:34 +0000

    New ToAttributedValueConverter to allow conversion of an element with string body and attributes (XSTR-344).
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1839 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/ToAttributedValueConverter.java
@@ -0,0 +1,305 @@
+/*
+ * Copyright (C) 2011 XStream Committers.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ * 
+ * Created on 30. July 2011 by Joerg Schaible
+ */
+
+package com.thoughtworks.xstream.converters.extended;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import com.thoughtworks.xstream.converters.ConversionException;
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.ConverterLookup;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.SingleValueConverter;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.converters.reflection.AbstractReflectionConverter.DuplicateFieldException;
+import com.thoughtworks.xstream.converters.reflection.ReflectionProvider;
+import com.thoughtworks.xstream.core.util.FastField;
+import com.thoughtworks.xstream.core.util.HierarchicalStreams;
+import com.thoughtworks.xstream.core.util.Primitives;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.mapper.Mapper;
+
+
+/**
+ * Converter that supports the definition of one field member that will be written as value and
+ * all other field members are written as attributes. The converter requires that all the field
+ * types (expect the one with the value) are handled by a {@link SingleValueConverter}. The
+ * value field is defined using the name of the type that declares the field and the field name
+ * itself. Therefore it is possible to define an inherited field as value. It is also possible
+ * to provide no value field at all, so that all fields are written as attributes.
+ * 
+ * @author J&ouml;rg Schaible
+ * @since upcoming
+ */
+public class ToAttributedValueConverter implements Converter {
+    private final Class type;
+    private final Mapper mapper;
+    private final ReflectionProvider reflectionProvider;
+    private final ConverterLookup lookup;
+    private final Field valueField;
+
+    /**
+     * Creates a new ToAttributedValueConverter instance.
+     * 
+     * @param mapper the mapper in use
+     * @param reflectionProvider the reflection provider in use
+     * @param lookup the converter lookup in use
+     * @param valueFieldName the field defining the tag's value (may be null)
+     */
+    public ToAttributedValueConverter(
+        final Class type, final Mapper mapper, final ReflectionProvider reflectionProvider,
+        final ConverterLookup lookup, final String valueFieldName) {
+        this(type, mapper, reflectionProvider, lookup, valueFieldName, null);
+    }
+
+    /**
+     * Creates a new ToAttributedValueConverter instance.
+     * 
+     * @param mapper the mapper in use
+     * @param reflectionProvider the reflection provider in use
+     * @param lookup the converter lookup in use
+     * @param valueFieldName the field defining the tag's value (may be null)
+     * @param valueDefinedIn the type defining the field
+     */
+    public ToAttributedValueConverter(
+        final Class type, final Mapper mapper, final ReflectionProvider reflectionProvider,
+        final ConverterLookup lookup, final String valueFieldName, Class valueDefinedIn) {
+        this.type = type;
+        this.mapper = mapper;
+        this.reflectionProvider = reflectionProvider;
+        this.lookup = lookup;
+
+        if (valueFieldName == null) {
+            valueField = null;
+        } else {
+            Field field = null;
+            try {
+                field = (valueDefinedIn != null ? valueDefinedIn : type)
+                    .getDeclaredField(valueFieldName);
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+            } catch (NoSuchFieldException e) {
+                throw new IllegalArgumentException(valueFieldName, e);
+            }
+            this.valueField = field;
+        }
+    }
+
+    public boolean canConvert(final Class type) {
+        return this.type == type;
+    }
+
+    public void marshal(final Object source, final HierarchicalStreamWriter writer,
+        final MarshallingContext context) {
+        final Class sourceType = source.getClass();
+        final Map defaultFieldDefinition = new HashMap();
+        final String[] tagValue = new String[1];
+        final Object[] realValue = new Object[1];
+        final Class[] fieldType = new Class[1];
+        final Class[] definingType = new Class[1];
+        reflectionProvider.visitSerializableFields(source, new ReflectionProvider.Visitor() {
+            public void visit(final String fieldName, final Class type, final Class definedIn,
+                final Object value) {
+                if (!mapper.shouldSerializeMember(definedIn, fieldName)) {
+                    return;
+                }
+
+                final FastField field = new FastField(definedIn, fieldName);
+                final String alias = mapper.serializedMember(definedIn, fieldName);
+                if (!defaultFieldDefinition.containsKey(alias)) {
+                    final Class lookupType = sourceType;
+                    defaultFieldDefinition.put(
+                        alias, reflectionProvider.getField(lookupType, fieldName));
+                } else if (!fieldIsEqual(field)) {
+                    final ConversionException exception = new ConversionException(
+                        "Cannot write attribute twice for object");
+                    exception.add("alias", alias);
+                    exception.add("type", sourceType.getName());
+                    throw exception;
+                }
+
+                Converter converter = mapper.getLocalConverter(definedIn, fieldName);
+                if (converter == null) {
+                    converter = lookup.lookupConverterForType(type);
+                }
+
+                if (value != null) {
+                    if (converter instanceof SingleValueConverter) {
+                        final String str = ((SingleValueConverter)converter).toString(value);
+
+                        if (valueField != null && fieldIsEqual(field)) {
+                            definingType[0] = definedIn;
+                            fieldType[0] = type;
+                            realValue[0] = value;
+                            tagValue[0] = str;
+                        } else {
+                            if (str != null) {
+                                writer.addAttribute(alias, str);
+                            }
+                        }
+                    } else {
+                        context.convertAnother(value);
+                    }
+                }
+            }
+        });
+
+        if (tagValue[0] != null) {
+            final Class actualType = realValue[0].getClass();
+            final Class defaultType = mapper.defaultImplementationOf(fieldType[0]);
+            if (!actualType.equals(defaultType)) {
+                final String serializedClassName = mapper.serializedClass(actualType);
+                if (!serializedClassName.equals(mapper.serializedClass(defaultType))) {
+                    final String attributeName = mapper.aliasForSystemAttribute("class");
+                    if (attributeName != null) {
+                        writer.addAttribute(attributeName, serializedClassName);
+                    }
+                }
+            }
+
+            writer.setValue(tagValue[0]);
+        }
+    }
+
+    public Object unmarshal(final HierarchicalStreamReader reader,
+        final UnmarshallingContext context) {
+        final Object result = reflectionProvider.newInstance(context.getRequiredType());
+        final Class resultType = result.getClass();
+
+        final Set seenFields = new HashSet<FastField>();
+        final Iterator it = reader.getAttributeNames();
+
+        final Set systemAttributes = new HashSet<String>();
+        systemAttributes.add(mapper.aliasForSystemAttribute("class"));
+
+        // Process attributes before recursing into child elements.
+        while (it.hasNext()) {
+            final String attrName = (String)it.next();
+            if (systemAttributes.contains(attrName)) {
+                continue;
+            }
+
+            final String fieldName = mapper.realMember(resultType, attrName);
+            final boolean fieldExistsInClass = reflectionProvider.fieldDefinedInClass(
+                fieldName, resultType);
+            if (fieldExistsInClass) {
+                final Field field = reflectionProvider.getField(resultType, fieldName);
+                if (Modifier.isTransient(field.getModifiers())) {
+                    continue;
+                }
+
+                Class type = field.getType();
+                final Class declaringClass = field.getDeclaringClass();
+                Converter converter = mapper.getLocalConverter(declaringClass, fieldName);
+                if (converter == null) {
+                    converter = lookup.lookupConverterForType(type);
+                }
+
+                if (!(converter instanceof SingleValueConverter)) {
+                    final ConversionException exception = new ConversionException(
+                        "Cannot read field as a single value for object");
+                    exception.add("field", fieldName);
+                    exception.add("type", resultType.getName());
+                    throw exception;
+                }
+
+                if (converter != null) {
+                    final Object value = ((SingleValueConverter)converter).fromString(reader
+                        .getAttribute(attrName));
+                    if (type.isPrimitive()) {
+                        type = Primitives.box(type);
+                    }
+
+                    if (value != null && !type.isAssignableFrom(value.getClass())) {
+                        final ConversionException exception = new ConversionException(
+                            "Cannot assign object to type");
+                        exception.add("object type", value.getClass().getName());
+                        exception.add("target type", type.getName());
+                        throw exception;
+                    }
+
+                    reflectionProvider.writeField(result, fieldName, value, declaringClass);
+                    if (!seenFields.add(new FastField(declaringClass, fieldName))) {
+                        throw new DuplicateFieldException(fieldName
+                            + " ["
+                            + declaringClass.getName()
+                            + "]");
+                    }
+                }
+            }
+        }
+
+        if (valueField != null) {
+            final Class classDefiningField = valueField.getDeclaringClass();
+            final String fieldName = valueField.getName();
+            if (fieldName == null
+                || !reflectionProvider.fieldDefinedInClass(fieldName, resultType)) {
+                final ConversionException exception = new ConversionException(
+                    "Cannot assign value to field of type");
+                exception.add("element", reader.getNodeName());
+                exception.add("field", fieldName);
+                exception.add("target type", context.getRequiredType().getName());
+                throw exception;
+            }
+
+            Class type;
+            final String classAttribute = HierarchicalStreams
+                .readClassAttribute(reader, mapper);
+            if (classAttribute != null) {
+                type = mapper.realClass(classAttribute);
+            } else {
+                type = mapper.defaultImplementationOf(reflectionProvider.getFieldType(
+                    result, fieldName, classDefiningField));
+            }
+
+            final Field field = reflectionProvider.getField(classDefiningField, fieldName);
+            final Object value = context.convertAnother(
+                result, type,
+                mapper.getLocalConverter(field.getDeclaringClass(), field.getName()));
+
+            final Class definedType = reflectionProvider.getFieldType(
+                result, fieldName, classDefiningField);
+            if (!definedType.isPrimitive()) {
+                type = definedType;
+            }
+
+            if (value != null && !type.isAssignableFrom(value.getClass())) {
+                final ConversionException exception = new ConversionException(
+                    "Cannot assign object to type");
+                exception.add("object type", value.getClass().getName());
+                exception.add("target type", type.getName());
+                throw exception;
+            }
+
+            reflectionProvider.writeField(result, fieldName, value, classDefiningField);
+            if (!seenFields.add(new FastField(classDefiningField, fieldName))) {
+                throw new DuplicateFieldException(fieldName
+                    + " ["
+                    + classDefiningField.getName()
+                    + "]");
+            }
+        }
+        return result;
+    }
+
+    private boolean fieldIsEqual(FastField field) {
+        return valueField.getName().equals(field.getName())
+            && valueField.getDeclaringClass().getName().equals(field.getDeclaringClass());
+    }
+}
---
+++ xstream/src/test/com/thoughtworks/xstream/converters/extended/ToAttributedValueConverterTest.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright (C) 2011 XStream Committers.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ * 
+ * Created on 30. July 2011 by Joerg Schaible
+ */
+
+package com.thoughtworks.xstream.converters.extended;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import com.thoughtworks.acceptance.objects.OpenSourceSoftware;
+import com.thoughtworks.acceptance.objects.Software;
+import com.thoughtworks.acceptance.someobjects.X;
+import com.thoughtworks.acceptance.someobjects.Y;
+import com.thoughtworks.xstream.converters.SingleValueConverterWrapper;
+import com.thoughtworks.xstream.converters.basic.IntConverter;
+import com.thoughtworks.xstream.converters.basic.StringConverter;
+import com.thoughtworks.xstream.converters.collections.ArrayConverter;
+import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
+import com.thoughtworks.xstream.converters.reflection.ReflectionProvider;
+import com.thoughtworks.xstream.converters.reflection.Sun14ReflectionProvider;
+import com.thoughtworks.xstream.core.DefaultConverterLookup;
+import com.thoughtworks.xstream.core.TreeMarshaller;
+import com.thoughtworks.xstream.core.TreeUnmarshaller;
+import com.thoughtworks.xstream.core.util.FastField;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.xml.CompactWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppReader;
+import com.thoughtworks.xstream.mapper.ArrayMapper;
+import com.thoughtworks.xstream.mapper.ClassAliasingMapper;
+import com.thoughtworks.xstream.mapper.DefaultImplementationsMapper;
+import com.thoughtworks.xstream.mapper.DefaultMapper;
+import com.thoughtworks.xstream.mapper.Mapper;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Tests {@link ToAttributedValueConverter}.
+ * 
+ * @author jos / last modified by $Author$
+ */
+public class ToAttributedValueConverterTest extends TestCase {
+    private DefaultConverterLookup converterLookup;
+    private ReflectionProvider reflectionProvider;
+    private Mapper mapper;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        final ClassAliasingMapper classAliasingMapper = new ClassAliasingMapper(
+            new DefaultMapper(getClass().getClassLoader()));
+        classAliasingMapper.addClassAlias("x", X.class);
+        classAliasingMapper.addClassAlias("software", Software.class);
+        classAliasingMapper.addClassAlias("open-source", OpenSourceSoftware.class);
+        mapper = new DefaultImplementationsMapper(new ArrayMapper(classAliasingMapper));
+
+        reflectionProvider = new Sun14ReflectionProvider();
+
+        converterLookup = new DefaultConverterLookup();
+        converterLookup.registerConverter(
+            new SingleValueConverterWrapper(new StringConverter()), 0);
+        converterLookup.registerConverter(
+            new SingleValueConverterWrapper(new IntConverter()), 0);
+        converterLookup.registerConverter(new ArrayConverter(mapper), 0);
+        converterLookup.registerConverter(
+            new ReflectionConverter(mapper, reflectionProvider), -1);
+    }
+
+    /**
+     * Tests conversion with field defined in converted class.
+     */
+    public void testWithValueInConvertedClass() {
+        converterLookup.registerConverter(new ToAttributedValueConverter(
+            Software.class, mapper, reflectionProvider, converterLookup, "name"), 0);
+
+        final Software name = new Software(null, "XStream");
+        final StringWriter writer = new StringWriter();
+        final CompactWriter compactWriter = new CompactWriter(writer);
+        new TreeMarshaller(compactWriter, converterLookup, mapper).start(name, null);
+        compactWriter.flush();
+        assertEquals("<software>XStream</software>", writer.toString());
+
+        final HierarchicalStreamReader reader = new XppReader(new StringReader(
+            writer.toString()));
+        assertEquals(
+            name, new TreeUnmarshaller(null, reader, converterLookup, mapper).start(null));
+    }
+
+    /**
+     * Tests conversion with field defined in superclass.
+     */
+    public void testWithValueInSuperclass() {
+        converterLookup.registerConverter(new ToAttributedValueConverter(
+            OpenSourceSoftware.class, mapper, reflectionProvider, converterLookup, "name",
+            Software.class), 0);
+
+        final Software software = new OpenSourceSoftware("Codehaus", "XStream", "BSD");
+        final StringWriter writer = new StringWriter();
+        final CompactWriter compactWriter = new CompactWriter(writer);
+        new TreeMarshaller(compactWriter, converterLookup, mapper).start(software, null);
+        compactWriter.flush();
+        assertEquals(
+            "<open-source vendor=\"Codehaus\" license=\"BSD\">XStream</open-source>",
+            writer.toString());
+
+        final HierarchicalStreamReader reader = new XppReader(new StringReader(
+            writer.toString()));
+        assertEquals(
+            software, new TreeUnmarshaller(null, reader, converterLookup, mapper).start(null));
+    }
+
+    /**
+     * Tests conversion distinguishes between different types.
+     */
+    public void testWillDistinguishBetweenDifferentTypes() {
+        converterLookup.registerConverter(new ToAttributedValueConverter(
+            Software.class, mapper, reflectionProvider, converterLookup, "name"), 0);
+        converterLookup.registerConverter(
+            new ToAttributedValueConverter(
+                OpenSourceSoftware.class, mapper, reflectionProvider, converterLookup,
+                "license"), 0);
+
+        final Software[] software = new Software[]{
+            new Software("Microsoft", "Windows"),
+            new OpenSourceSoftware("Codehaus", "XStream", "BSD")};
+        final StringWriter writer = new StringWriter();
+        final PrettyPrintWriter prettyPrintWriter = new PrettyPrintWriter(writer);
+        new TreeMarshaller(prettyPrintWriter, converterLookup, mapper).start(software, null);
+        prettyPrintWriter.flush();
+        assertEquals(""
+            + "<software-array>\n"
+            + "  <software vendor=\"Microsoft\">Windows</software>\n"
+            + "  <open-source vendor=\"Codehaus\" name=\"XStream\">BSD</open-source>\n"
+            + "</software-array>", writer.toString());
+
+        final HierarchicalStreamReader reader = new XppReader(new StringReader(
+            writer.toString()));
+        Software[] array = (Software[])new TreeUnmarshaller(
+            null, reader, converterLookup, mapper).start(null);
+        assertEquals(software[0], array[0]);
+        assertEquals(software[1], array[1]);
+    }
+
+    /**
+     * Tests conversion with null in field value.
+     */
+    public void testWithNullValueDeserializedAsEmptyString() {
+        converterLookup.registerConverter(new ToAttributedValueConverter(
+            Software.class, mapper, reflectionProvider, converterLookup, "name"), 0);
+
+        final Software software = new Software(null, null);
+        final StringWriter writer = new StringWriter();
+        final CompactWriter compactWriter = new CompactWriter(writer);
+        new TreeMarshaller(compactWriter, converterLookup, mapper).start(software, null);
+        compactWriter.flush();
+        assertEquals("<software/>", writer.toString());
+
+        final HierarchicalStreamReader reader = new XppReader(new StringReader(
+            writer.toString()));
+        assertEquals(
+            "",
+            ((Software)new TreeUnmarshaller(null, reader, converterLookup, mapper).start(null)).name);
+    }
+
+    /**
+     * Tests conversion with null in field value.
+     */
+    public void testWithoutValueField() {
+        converterLookup.registerConverter(new ToAttributedValueConverter(
+            Software.class, mapper, reflectionProvider, converterLookup, null), 0);
+
+        final Software software = new Software("Codehaus", "XStream");
+        final StringWriter writer = new StringWriter();
+        final CompactWriter compactWriter = new CompactWriter(writer);
+        new TreeMarshaller(compactWriter, converterLookup, mapper).start(software, null);
+        compactWriter.flush();
+        assertEquals("<software vendor=\"Codehaus\" name=\"XStream\"/>", writer.toString());
+
+        final HierarchicalStreamReader reader = new XppReader(new StringReader(
+            writer.toString()));
+        assertEquals(
+            software, new TreeUnmarshaller(null, reader, converterLookup, mapper).start(null));
+    }
+
+    /**
+     * Tests conversion with complex value field.
+     */
+    public void testWithComplexValueField() {
+        converterLookup.registerConverter(new ToAttributedValueConverter(
+            X.class, mapper, reflectionProvider, converterLookup, "innerObj"), 0);
+
+        final X x = new X(42);
+        x.aStr = "xXx";
+        x.innerObj = new Y();
+        x.innerObj.yField = "inner";
+        final StringWriter writer = new StringWriter();
+        final CompactWriter compactWriter = new CompactWriter(writer);
+        new TreeMarshaller(compactWriter, converterLookup, mapper).start(x, null);
+        compactWriter.flush();
+        assertEquals(
+            "<x aStr=\"xXx\" anInt=\"42\"><yField>inner</yField></x>", writer.toString());
+
+        final HierarchicalStreamReader reader = new XppReader(new StringReader(
+            writer.toString()));
+        assertEquals(x, new TreeUnmarshaller(null, reader, converterLookup, mapper).start(null));
+    }
+}
