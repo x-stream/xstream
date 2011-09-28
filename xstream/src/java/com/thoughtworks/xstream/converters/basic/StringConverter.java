d2f4bc97ed00db070bd34ae4cb5f109192e61392
Jörg Schaible <joerg.schaible@gmx.de>
2011-09-28 21:03:24 +0000

    Add length limit for cached strings in StringConverter, 38 characters by default.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1892 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/basic/StringConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/basic/StringConverter.java
@@ -21,37 +21,63 @@ import com.thoughtworks.xstream.core.util.WeakCache;
  * Converts a String to a String ;).
  * <p>
  * Well ok, it doesn't <i>actually</i> do any conversion. The converter uses by default a map
- * with weak references to reuse instances.
+ * with weak references to reuse instances of strings that do not exceed a length limit. This
+ * limit is by default 38 characters to cache typical strings containing UUIDs. Only shorter
+ * strings are typically repeated more often in XML values.
  * </p>
  * 
  * @author Joe Walnes
  * @author Rene Schwietzke
  * @author J&ouml;rg Schaible
- * @see String#intern()
  */
 public class StringConverter extends AbstractSingleValueConverter {
 
+    private static final int LENGTH_LIMIT = 38;
+    
     /**
      * A Map to store strings as long as needed to map similar strings onto the same instance
      * and conserve memory. The map can be set from the outside during construction, so it can
      * be a LRU map or a weak map, synchronised or not.
      */
     private final Map cache;
+    private final int lengthLimit;
 
     /**
-     * Construct a StringConverter using a cache with weak references.
+     * Construct a StringConverter using a map-based cache for strings not exceeding the length limit.
+     * 
+     * @param map the map to use for the instances to reuse (may be null to not cache at all)
+     * @param lengthLimit maximum string length of a cached string, -1 to cache all, 0 to turn off the cache
+     * @since upcoming
+     */
+    public StringConverter(final Map map, int lengthLimit) {
+        cache = map;
+        this.lengthLimit = lengthLimit;
+    }
+
+    /**
+     * Construct a StringConverter using a map-based cache for strings not exceeding 38 characters.
      * 
      * @param map the map to use for the instances to reuse (may be null to not cache at all)
      */
     public StringConverter(final Map map) {
-        cache = map;
+        this(map, LENGTH_LIMIT);
     }
 
     /**
-     * Construct a StringConverter using a cache with weak references.
+     * Construct a StringConverter using a cache with weak references for strings not exceeding the length limit.
+     * 
+     * @param lengthLimit maximum string length of a cached string, -1 to cache all, 0 to turn off the cache
+     * @since upcoming
+     */
+    public StringConverter(int lengthLimit) {
+        this(Collections.synchronizedMap(new WeakCache()), lengthLimit);
+    }
+
+    /**
+     * Construct a StringConverter using a cache with weak references for strings not exceeding 38 characters.
      */
     public StringConverter() {
-        this(Collections.synchronizedMap(new WeakCache()));
+        this(LENGTH_LIMIT);
     }
 
     public boolean canConvert(final Class type) {
@@ -59,7 +85,7 @@ public class StringConverter extends AbstractSingleValueConverter {
     }
 
     public Object fromString(final String str) {
-        if (cache != null) {
+        if (cache != null && str != null && (lengthLimit < 0 || str.length() <= lengthLimit)) {
             String s = (String)cache.get(str);
 
             if (s == null) {
