a15bb11de677cdc165f77594d7c5511c90640028
Jörg Schaible <joerg.schaible@gmx.de>
2011-09-30 22:26:45 +0000

    Code format, fix copyright years.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1904 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/io/xml/StaxWriter.java
+++ xstream/src/java/com/thoughtworks/xstream/io/xml/StaxWriter.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2004, 2005, 2006 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2009, 201, 20110 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -18,14 +18,15 @@ import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
+
 /**
  * A stream writing that outputs to a StAX stream writer
- *
+ * 
  * @author James Strachan
  * @version $Revision$
  */
 public class StaxWriter extends AbstractXmlWriter {
-    
+
     private final QNameMap qnameMap;
     private final XMLStreamWriter out;
     private final boolean writeEnclosingDocument;
@@ -37,34 +38,35 @@ public class StaxWriter extends AbstractXmlWriter {
         this(qnameMap, out, true, true);
     }
 
-
     /**
      * Allows a StaxWriter to be created for partial XML output
-     *
-     * @param qnameMap               is the mapper of Java class names to QNames
-     * @param out                    the stream to output to
-     * @param nameCoder              the xml-friendly replacer to escape Java names
+     * 
+     * @param qnameMap is the mapper of Java class names to QNames
+     * @param out the stream to output to
+     * @param nameCoder the xml-friendly replacer to escape Java names
      * @throws XMLStreamException if the events could not be written to the output
      * @since 1.4
      */
-    public StaxWriter(QNameMap qnameMap, XMLStreamWriter out, NameCoder nameCoder) throws XMLStreamException {
+    public StaxWriter(QNameMap qnameMap, XMLStreamWriter out, NameCoder nameCoder)
+        throws XMLStreamException {
         this(qnameMap, out, true, true, nameCoder);
     }
 
     /**
      * Allows a StaxWriter to be created for partial XML output
-     *
-     * @param qnameMap               is the mapper of Java class names to QNames
-     * @param out                    the stream to output to
-     * @param writeEnclosingDocument a flag to indicate whether or not the start/end document events should be written
+     * 
+     * @param qnameMap is the mapper of Java class names to QNames
+     * @param out the stream to output to
+     * @param writeEnclosingDocument a flag to indicate whether or not the start/end document
+     *            events should be written
      * @param namespaceRepairingMode a flag to enable StAX' namespace repairing mode
-     * @param nameCoder              the xml-friendly replacer to escape Java names
+     * @param nameCoder the xml-friendly replacer to escape Java names
      * @throws XMLStreamException if the events could not be written to the output
      * @since 1.4
      */
-    public StaxWriter(QNameMap qnameMap, XMLStreamWriter out, 
-            boolean writeEnclosingDocument, boolean namespaceRepairingMode,
-            NameCoder nameCoder) throws XMLStreamException {
+    public StaxWriter(
+        QNameMap qnameMap, XMLStreamWriter out, boolean writeEnclosingDocument,
+        boolean namespaceRepairingMode, NameCoder nameCoder) throws XMLStreamException {
         super(nameCoder);
         this.qnameMap = qnameMap;
         this.out = out;
@@ -77,38 +79,45 @@ public class StaxWriter extends AbstractXmlWriter {
 
     /**
      * Allows a StaxWriter to be created for partial XML output
-     *
-     * @param qnameMap               is the mapper of Java class names to QNames
-     * @param out                    the stream to output to
-     * @param writeEnclosingDocument a flag to indicate whether or not the start/end document events should be written
+     * 
+     * @param qnameMap is the mapper of Java class names to QNames
+     * @param out the stream to output to
+     * @param writeEnclosingDocument a flag to indicate whether or not the start/end document
+     *            events should be written
      * @throws XMLStreamException if the events could not be written to the output
      */
-    public StaxWriter(QNameMap qnameMap, XMLStreamWriter out, boolean writeEnclosingDocument, boolean namespaceRepairingMode) throws XMLStreamException {
-        this(qnameMap, out, writeEnclosingDocument, namespaceRepairingMode, new XmlFriendlyNameCoder());
+    public StaxWriter(
+        QNameMap qnameMap, XMLStreamWriter out, boolean writeEnclosingDocument,
+        boolean namespaceRepairingMode) throws XMLStreamException {
+        this(
+            qnameMap, out, writeEnclosingDocument, namespaceRepairingMode,
+            new XmlFriendlyNameCoder());
     }
 
     /**
      * Allows a StaxWriter to be created for partial XML output
-     *
-     * @param qnameMap               is the mapper of Java class names to QNames
-     * @param out                    the stream to output to
-     * @param writeEnclosingDocument a flag to indicate whether or not the start/end document events should be written
-     * @param replacer              the xml-friendly replacer to escape Java names
+     * 
+     * @param qnameMap is the mapper of Java class names to QNames
+     * @param out the stream to output to
+     * @param writeEnclosingDocument a flag to indicate whether or not the start/end document
+     *            events should be written
+     * @param replacer the xml-friendly replacer to escape Java names
      * @throws XMLStreamException if the events could not be written to the output
      * @since 1.2
-     * @deprecated As of 1.4 use {@link StaxWriter#StaxWriter(QNameMap, XMLStreamWriter, boolean, boolean, NameCoder)} instead
+     * @deprecated As of 1.4 use
+     *             {@link StaxWriter#StaxWriter(QNameMap, XMLStreamWriter, boolean, boolean, NameCoder)}
+     *             instead
      */
-    public StaxWriter(QNameMap qnameMap, XMLStreamWriter out, 
-            boolean writeEnclosingDocument, boolean namespaceRepairingMode,
-            XmlFriendlyReplacer replacer) throws XMLStreamException {
+    public StaxWriter(
+        QNameMap qnameMap, XMLStreamWriter out, boolean writeEnclosingDocument,
+        boolean namespaceRepairingMode, XmlFriendlyReplacer replacer) throws XMLStreamException {
         this(qnameMap, out, writeEnclosingDocument, namespaceRepairingMode, (NameCoder)replacer);
     }
-    
+
     public void flush() {
         try {
             out.flush();
-        }
-        catch (XMLStreamException e) {
+        } catch (XMLStreamException e) {
             throw new StreamException(e);
         }
     }
@@ -119,8 +128,7 @@ public class StaxWriter extends AbstractXmlWriter {
     public void close() {
         try {
             out.close();
-        }
-        catch (XMLStreamException e) {
+        } catch (XMLStreamException e) {
             throw new StreamException(e);
         }
     }
@@ -128,21 +136,19 @@ public class StaxWriter extends AbstractXmlWriter {
     public void addAttribute(String name, String value) {
         try {
             out.writeAttribute(encodeAttribute(name), value);
-        }
-        catch (XMLStreamException e) {
+        } catch (XMLStreamException e) {
             throw new StreamException(e);
         }
     }
 
     public void endNode() {
         try {
-            tagDepth--;
+            tagDepth-- ;
             out.writeEndElement();
             if (tagDepth == 0 && writeEnclosingDocument) {
                 out.writeEndDocument();
             }
-        }
-        catch (XMLStreamException e) {
+        } catch (XMLStreamException e) {
             throw new StreamException(e);
         }
     }
@@ -150,8 +156,7 @@ public class StaxWriter extends AbstractXmlWriter {
     public void setValue(String text) {
         try {
             out.writeCharacters(text);
-        }
-        catch (XMLStreamException e) {
+        } catch (XMLStreamException e) {
             throw new StreamException(e);
         }
     }
@@ -175,8 +180,7 @@ public class StaxWriter extends AbstractXmlWriter {
                     if (currentNamespace == null || !currentNamespace.equals(uri)) {
                         writeNamespace = true;
                     }
-                }
-                else {
+                } else {
                     String defaultNamespace = out.getNamespaceContext().getNamespaceURI("");
                     if (defaultNamespace == null || !defaultNamespace.equals(uri)) {
                         writeNamespace = true;
@@ -186,8 +190,7 @@ public class StaxWriter extends AbstractXmlWriter {
 
             if (hasPrefix) {
                 out.setPrefix(prefix, uri);
-            }
-            else if (hasURI) {
+            } else if (hasURI) {
                 if (writeNamespace) {
                     out.setDefaultNamespace(uri);
                 }
@@ -196,14 +199,12 @@ public class StaxWriter extends AbstractXmlWriter {
             if (hasURI && writeNamespace && !isNamespaceRepairingMode()) {
                 if (hasPrefix) {
                     out.writeNamespace(prefix, uri);
-                }
-                else {
+                } else {
                     out.writeDefaultNamespace(uri);
                 }
             }
-            tagDepth++;
-        }
-        catch (XMLStreamException e) {
+            tagDepth++ ;
+        } catch (XMLStreamException e) {
             throw new StreamException(e);
         }
     }
