a645a9840b519d044da00e66966d83808b5a7348
Jörg Schaible <joerg.schaible@gmx.de>
2012-02-23 23:16:14 +0000

    FieldDictionary may call sort of FieldKeySorter implementation with wrong type as key.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1961 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/reflection/FieldDictionary.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/reflection/FieldDictionary.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2004, 2005, 2006 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -160,7 +160,7 @@ public class FieldDictionary implements Caching {
                             }
                             keyedByFieldKey.put(fieldKey, field);
                         }
-                        final Map sortedFieldKeys = sorter.sort(type, keyedByFieldKey);
+                        final Map sortedFieldKeys = sorter.sort(cls, keyedByFieldKey);
                         keyedByFieldNameCache.put(cls, keyedByFieldName);
                         keyedByFieldKeyCache.put(cls, sortedFieldKeys);
                         lastKeyedByFieldName = keyedByFieldName;
