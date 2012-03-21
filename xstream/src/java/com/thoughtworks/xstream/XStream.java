2ee89811cef507fc854132c8c9a889e603fac9b0
Jörg Schaible <joerg.schaible@gmx.de>
2012-03-21 01:17:55 +0000

    Clean-up.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1968 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/XStream.java
+++ xstream/src/java/com/thoughtworks/xstream/XStream.java
@@ -618,7 +618,7 @@ public class XStream {
 
         if (JVM.is15()) {
             aliasDynamically("duration", "javax.xml.datatype.Duration");
-            aliasDynamically("concurrent-hash-map", "java.util.concurrent.ConcurrentHashMap");
+            alias("concurrent-hash-map", jvm.loadClass("java.util.concurrent.ConcurrentHashMap"));
             alias("enum-set", jvm.loadClass("java.util.EnumSet"));
             alias("enum-map", jvm.loadClass("java.util.EnumMap"));
             alias("string-builder", jvm.loadClass("java.lang.StringBuilder"));
