66bde074e0e35e3d61993b008c6e3779faba57ea
Jörg Schaible <joerg.schaible@gmx.de>
2012-11-01 17:07:16 +0000

    Remove unused import.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@1996 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/mapper/AnnotationMapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/AnnotationMapper.java
@@ -19,7 +19,6 @@ import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.BitSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
