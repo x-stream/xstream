cd83e4c3832cca1f3931a0ff96e53b841ec38fc2
Jörg Schaible <joerg.schaible@gmx.de>
2013-03-13 18:38:18 +0000

    Typo.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@2037 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/xstream/converters/reflection/PureJavaReflectionProvider15Test.java
+++ xstream/src/test/com/thoughtworks/xstream/converters/reflection/PureJavaReflectionProvider15Test.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006, 2007 XStream Committers.
+ * Copyright (C) 2006, 2007, 2013 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -33,7 +33,7 @@ public class PureJavaReflectionProvider15Test extends AbstractReflectionProvider
         }
     }
 
-    public void testCanCreateWithFinalFIeld() {
+    public void testCanCreateWithFinalField() {
         assertCanCreate(WithFinalField.class);
     }
 
