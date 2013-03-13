3328e175054605acba31505c5fb4b4304f172195
Jörg Schaible <joerg.schaible@gmx.de>
2013-03-13 18:18:42 +0000

    Cache class name lookup failures (XSTR-724).
    git-svn-id: http://svn.codehaus.org/xstream/trunk@2036 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/mapper/CachingMapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/CachingMapper.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2005 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2009, 2011 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2011, 2013 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -11,7 +11,6 @@
  */
 package com.thoughtworks.xstream.mapper;
 
-import java.lang.ref.WeakReference;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,17 +33,22 @@ public class CachingMapper extends MapperWrapper implements Caching {
     }
 
     public Class realClass(String elementName) {
-        WeakReference reference = (WeakReference) realClassCache.get(elementName);
-        if (reference != null) {
-            Class cached = (Class) reference.get();
-            if (cached != null) {
-                return cached;
+        Object cached = realClassCache.get(elementName);
+        if (cached != null) {
+            if (cached instanceof Class) {
+                return (Class)cached;
             }
+            throw (CannotResolveClassException)cached;
         }
-        
-        Class result = super.realClass(elementName);
-        realClassCache.put(elementName, new WeakReference(result));
-        return result;
+
+        try {
+            Class result = super.realClass(elementName);
+            realClassCache.put(elementName, result);
+            return result;
+        } catch (CannotResolveClassException e) {
+            realClassCache.put(elementName, e);
+            throw e;
+        }
     }
 
     public void flushCache() {
