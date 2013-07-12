396ff25953ffb3f8d4efc593dd49fa28d6f20ea3
Jörg Schaible <joerg.schaible@gmx.de>
2013-07-12 17:18:59 +0000

    Improve javadoc.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@2098 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/annotations/XStreamAlias.java
+++ xstream/src/java/com/thoughtworks/xstream/annotations/XStreamAlias.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2005 Joe Walnes.
- * Copyright (C) 2006, 2007 XStream Committers.
+ * Copyright (C) 2006, 2007, 2013 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -17,17 +17,23 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation used to define an XStream class or field value.
+ * Annotation used to define an XStream class or field alias.
  *
  * @author Emil Kirschner
  * @author Chung-Onn Cheong
+ * @see com.thoughtworks.xstream.XStream#alias(String, Class)
+ * @see com.thoughtworks.xstream.XStream#alias(String, Class, Class)
+ * @see com.thoughtworks.xstream.XStream#addDefaultImplementation(Class, Class)
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD})
 public @interface XStreamAlias {
     /**
-     * The value of the class or field value
+     * The name of the class or field alias.
      */
     public String value();
+    /**
+     * A possible default implementation if the annotated type is an interface.
+     */
     public Class<?> impl() default Void.class; //Use Void to denote as Null
 }
