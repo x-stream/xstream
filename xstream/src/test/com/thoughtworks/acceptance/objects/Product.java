ea9263b9c892c669750329a3d52dc33cb0123ab1
Jörg Schaible <joerg.schaible@gmx.de>
2013-09-17 22:29:49 +0000

    Implement equals and hashCode for Product.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@2119 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/test/com/thoughtworks/acceptance/objects/Product.java
+++ xstream/src/test/com/thoughtworks/acceptance/objects/Product.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 XStream Committers.
+ * Copyright (C) 2007, 2013 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -57,6 +57,37 @@ public class Product {
 
     public void setTags(ArrayList tags) {
         this.tags = tags;
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(this.price);
+        result = prime * result + (int)(temp ^ (temp >>> 32));
+        result = prime * result + ((this.tags == null) ? 0 : this.tags.hashCode());
+        return result;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Product other = (Product)obj;
+        if (this.id == null) {
+            if (other.id != null) return false;
+        } else if (!this.id.equals(other.id)) return false;
+        if (this.name == null) {
+            if (other.name != null) return false;
+        } else if (!this.name.equals(other.name)) return false;
+        if (Double.doubleToLongBits(this.price) != Double.doubleToLongBits(other.price))
+            return false;
+        if (this.tags == null) {
+            if (other.tags != null) return false;
+        } else if (!this.tags.equals(other.tags)) return false;
+        return true;
     }
 
     public String toString() {
