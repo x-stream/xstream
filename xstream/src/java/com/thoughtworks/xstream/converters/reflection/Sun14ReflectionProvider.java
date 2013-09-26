99e7404b5e0ea436e56bbf4fecd0db1fa13fc8e0
Jörg Schaible <joerg.schaible@gmx.de>
2013-09-26 19:59:23 +0000

    git-svn-id: http://svn.codehaus.org/xstream/trunk@2138 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/reflection/Sun14ReflectionProvider.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/reflection/Sun14ReflectionProvider.java
@@ -52,9 +52,11 @@ public class Sun14ReflectionProvider extends PureJavaReflectionProvider {
     }
 
     public Sun14ReflectionProvider() {
+        super();
     }
 
     public Sun14ReflectionProvider(FieldDictionary dic) {
+        super(dic);
     }
 
     public Object newInstance(Class type) {
