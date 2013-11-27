b389cffa37394e83b4f8ac76e33ca6f65bab8f70
Jörg Schaible <joerg.schaible@gmx.de>
2013-11-27 01:19:35 +0000

    We have Java 1.4 as minimum. No need for reflection in StackTraceElementFactory anymore.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@2157 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/extended/StackTraceElementFactory.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/StackTraceElementFactory.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2004 Joe Walnes.
- * Copyright (C) 2006, 2007 XStream Committers.
+ * Copyright (C) 2006, 2007, 2013 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -11,12 +11,7 @@
  */
 package com.thoughtworks.xstream.converters.extended;
 
-import com.thoughtworks.xstream.converters.ConversionException;
-
-import java.lang.reflect.Field;
-
 /**
- * Factory for creating StackTraceElements.
  * Factory for creating StackTraceElements.
  *
  * @author <a href="mailto:boxley@thoughtworks.com">B. K. Oxley (binkley)</a>
@@ -41,22 +36,6 @@ public class StackTraceElementFactory {
     }
 
     private StackTraceElement create(String declaringClass, String methodName, String fileName, int lineNumber) {
-        StackTraceElement result = new Throwable().getStackTrace()[0];
-        setField(result, "declaringClass", declaringClass);
-        setField(result, "methodName", methodName);
-        setField(result, "fileName", fileName);
-        setField(result, "lineNumber", new Integer(lineNumber));
-        return result;
+        return new StackTraceElement(declaringClass, methodName, fileName, lineNumber);
     }
-
-    private void setField(StackTraceElement element, String fieldName, Object value) {
-        try {
-            final Field field = StackTraceElement.class.getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(element, value);
-        } catch (Exception e) {
-            throw new ConversionException(e);
-        }
-    }
-
 }
