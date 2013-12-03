48ae7148aba42f6ab5183553984cdae76fd7fe15
Jörg Schaible <joerg.schaible@gmx.de>
2013-12-03 22:41:56 +0000

    Fix license.
    git-svn-id: http://svn.codehaus.org/xstream/branches/v-1.4.x@2174 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/extended/StackTraceElementFactory15.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/StackTraceElementFactory15.java
@@ -2,6 +2,10 @@
  * Copyright (C) 2013 XStream Committers.
  * All rights reserved.
  *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ *
  * Created on 03. December 2013 by Joerg Schaible
  */
 package com.thoughtworks.xstream.converters.extended;
