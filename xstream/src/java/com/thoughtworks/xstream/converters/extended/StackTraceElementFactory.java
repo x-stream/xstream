4752e762fe30a7a535a1e63f82f0d3906b06b8ae
Jörg Schaible <joerg.schaible@gmx.de>
2013-12-03 00:11:14 +0000

    Fix usage of StackTraceElement constructor, that is not yet available for Java 1.4.
    git-svn-id: http://svn.codehaus.org/xstream/branches/v-1.4.x@2166 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/extended/StackTraceElementFactory.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/extended/StackTraceElementFactory.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2004 Joe Walnes.
- * Copyright (C) 2006, 2007, 2013 XStream Committers.
+ * Copyright (C) 2006, 2007 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -11,7 +11,12 @@
  */
 package com.thoughtworks.xstream.converters.extended;
 
+import com.thoughtworks.xstream.converters.ConversionException;
+
+import java.lang.reflect.Field;
+
 /**
+ * Factory for creating StackTraceElements.
  * Factory for creating StackTraceElements.
  *
  * @author <a href="mailto:boxley@thoughtworks.com">B. K. Oxley (binkley)</a>
@@ -36,6 +41,22 @@ public class StackTraceElementFactory {
     }
 
     private StackTraceElement create(String declaringClass, String methodName, String fileName, int lineNumber) {
-        return new StackTraceElement(declaringClass, methodName, fileName, lineNumber);
+        StackTraceElement result = new Throwable().getStackTrace()[0];
+        setField(result, "declaringClass", declaringClass);
+        setField(result, "methodName", methodName);
+        setField(result, "fileName", fileName);
+        setField(result, "lineNumber", new Integer(lineNumber));
+        return result;
     }
+
+    private void setField(StackTraceElement element, String fieldName, Object value) {
+        try {
+            final Field field = StackTraceElement.class.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(element, value);
+        } catch (Exception e) {
+            throw new ConversionException(e);
+        }
+    }
+
 }
