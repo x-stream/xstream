ac11594af7ef186cae53d459dd9e0e6d4b5caec5
Jörg Schaible <joerg.schaible@gmx.de>
2013-12-22 00:03:18 +0000

    Fix copyright year.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@2196 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/mapper/MapperWrapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/MapperWrapper.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2005, 2006 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2009 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2013 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
