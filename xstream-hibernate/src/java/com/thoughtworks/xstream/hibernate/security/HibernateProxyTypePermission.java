eac41ef3bbcc847fce71eaf21146e98a877a5477
Jörg Schaible <joerg.schaible@gmx.de>
2014-01-19 12:07:15 +0000

    Add permissions for Java, CGLIB and Hibernate proxies.
    git-svn-id: http://svn.codehaus.org/xstream/trunk@2216 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

---
+++ xstream-hibernate/src/java/com/thoughtworks/xstream/hibernate/security/HibernateProxyTypePermission.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 XStream Committers.
+ * All rights reserved.
+ *
+ * Created on 19. January 2014 by Joerg Schaible
+ */
+package com.thoughtworks.xstream.hibernate.security;
+
+import org.hibernate.proxy.HibernateProxy;
+
+import com.thoughtworks.xstream.security.TypePermission;
+
+
+/**
+ * Permission for any array type.
+ * 
+ * @author J&ouml;rg Schaible
+ * @since upcoming
+ */
+public class HibernateProxyTypePermission implements TypePermission {
+    /**
+     * @since upcoming
+     */
+    public static final TypePermission PROXIES = new HibernateProxyTypePermission();
+
+    @Override
+    public boolean allows(final Class<?> type) {
+        return type != null && HibernateProxy.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return obj != null && obj.getClass() == HibernateProxyTypePermission.class;
+    }
+}
---
+++ xstream/src/java/com/thoughtworks/xstream/security/CGLIBProxyTypePermission.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 XStream Committers.
+ * All rights reserved.
+ *
+ * Created on 19. January 2014 by Joerg Schaible
+ */
+package com.thoughtworks.xstream.security;
+
+import net.sf.cglib.proxy.Proxy;
+
+
+/**
+ * Permission for any array type.
+ * 
+ * @author J&ouml;rg Schaible
+ * @since upcoming
+ */
+public class CGLIBProxyTypePermission implements TypePermission {
+    /**
+     * @since upcoming
+     */
+    public static final TypePermission PROXIES = new CGLIBProxyTypePermission();
+
+    @Override
+    public boolean allows(final Class<?> type) {
+        return type != null && Proxy.isProxyClass(type);
+    }
+
+    @Override
+    public int hashCode() {
+        return 19;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return obj != null && obj.getClass() == CGLIBProxyTypePermission.class;
+    }
+}
