9924063c70501ef2edeaec2d71f475768d10816e
Jörg Schaible <joerg.schaible@gmx.de>
2014-04-08 20:47:51 +0000

    Fix copyright.
    git-svn-id: http://svn.codehaus.org/xstream/branches/v-1.4.x@2282 9830eeb5-ddf4-0310-9ef7-f4b9a3e3227e

--- xstream/src/java/com/thoughtworks/xstream/converters/reflection/ExternalizableConverter.java
+++ xstream/src/java/com/thoughtworks/xstream/converters/reflection/ExternalizableConverter.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2004, 2005, 2006 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2010, 2011, 2013 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2010, 2011, 2013, 2014 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
