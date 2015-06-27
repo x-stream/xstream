90f29fae1f1368ff78514af5cf716aec312f0c33
joehni <joerg.schaible@gmx.de>
2015-06-27 17:22:00 +0200

    Use ConcurrentHashMap in CachingMapper (XSTR-727).

--- xstream/src/java/com/thoughtworks/xstream/mapper/CachingMapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/CachingMapper.java
@@ -1,19 +1,18 @@
 /*
  * Copyright (C) 2005 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2009, 2011, 2013, 2014 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2011, 2013, 2014, 2015 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
- * 
+ *
  * Created on 22. January 2005 by Joe Walnes
  */
 package com.thoughtworks.xstream.mapper;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import com.thoughtworks.xstream.XStreamException;
 import com.thoughtworks.xstream.core.Caching;
@@ -22,13 +21,13 @@ import com.thoughtworks.xstream.security.ForbiddenClassException;
 
 /**
  * Mapper that caches which names map to which classes. Prevents repetitive searching and class loading.
- * 
+ *
  * @author Joe Walnes
  * @author J&ouml;rg Schaible
  */
 public class CachingMapper extends MapperWrapper implements Caching {
 
-    private transient Map<String, ? super Object> realClassCache;
+    private transient ConcurrentMap<String, ? super Object> realClassCache;
 
     public CachingMapper(final Mapper wrapped) {
         super(wrapped);
@@ -46,14 +45,13 @@ public class CachingMapper extends MapperWrapper implements Caching {
         }
 
         try {
-            final Class<?> result = super.realClass(elementName);
-            realClassCache.put(elementName, result);
-            return result;
+            realClassCache.putIfAbsent(elementName, super.realClass(elementName));
+            return (Class<?>)realClassCache.get(elementName);
         } catch (final ForbiddenClassException e) {
-            realClassCache.put(elementName, e);
+            realClassCache.putIfAbsent(elementName, e);
             throw e;
         } catch (final CannotResolveClassException e) {
-            realClassCache.put(elementName, e);
+            realClassCache.putIfAbsent(elementName, e);
             throw e;
         }
     }
@@ -64,7 +62,7 @@ public class CachingMapper extends MapperWrapper implements Caching {
     }
 
     private Object readResolve() {
-        realClassCache = Collections.synchronizedMap(new HashMap<String, Object>(128));
+        realClassCache = new ConcurrentHashMap<String, Object>(128);
         return this;
     }
 }
