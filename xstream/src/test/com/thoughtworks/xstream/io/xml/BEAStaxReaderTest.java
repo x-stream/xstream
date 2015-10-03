8c1cc21967dab27660797d07006d7db248cf7ce9
joehni <joerg.schaible@gmx.de>
2015-10-03 17:57:42 +0200

    Improve comment.

--- xstream/src/test/com/thoughtworks/xstream/io/xml/BEAStaxReaderTest.java
+++ xstream/src/test/com/thoughtworks/xstream/io/xml/BEAStaxReaderTest.java
@@ -5,42 +5,32 @@
  * The software in this package is published under the terms of the BSD
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
- * 
+ *
  * Created on 30. September 2011 by Joerg Schaible
  */
 package com.thoughtworks.xstream.io.xml;
 
-import com.thoughtworks.xstream.XStreamException;
+import java.io.StringReader;
+
 import com.thoughtworks.xstream.io.HierarchicalStreamDriver;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 
-import java.io.StringReader;
 
 public class BEAStaxReaderTest extends AbstractXMLReaderTest {
-    
-    private HierarchicalStreamDriver driver = new BEAStaxDriver();
+
+    private final HierarchicalStreamDriver driver = new BEAStaxDriver();
 
     // factory method
     @Override
-    protected HierarchicalStreamReader createReader(String xml) throws Exception {
+    protected HierarchicalStreamReader createReader(final String xml) throws Exception {
         return driver.createReader(new StringReader(xml));
     }
 
     @Override
     public void testIsXXEVulnerable() throws Exception {
-        // Implementation ignores XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES
-    }
-
-    public void todoIsXXEVulnerable() throws Exception {
-        try {
-            super.testIsXXEVulnerable();
-            fail("Thrown " + XStreamException.class.getName() + " expected");
-        } catch (final XStreamException e) {
-            final String message = e.getMessage();
-            if (message.contains("Package")) {
-                throw e;
-            }
-        }
+        // Implementation wrongly claims not to handle external entities.
+        // On top it ignores XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES !!
+        // super.testIsXXEVulnerable();
     }
 
     // inherits tests from superclass
