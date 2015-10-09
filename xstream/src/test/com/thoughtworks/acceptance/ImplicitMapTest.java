01f85db4e740605e4022db4a42fc75c4e7ed1203
joehni <joerg.schaible@gmx.de>
2015-10-09 09:29:24 +0200

    Fix undefined order of map elements in unit test.

--- xstream/src/test/com/thoughtworks/acceptance/ImplicitMapTest.java
+++ xstream/src/test/com/thoughtworks/acceptance/ImplicitMapTest.java
@@ -494,6 +494,7 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
     
     public void testWithHiddenMap() {
         SampleMaps2 sample = new SampleMaps2();
+        ((SampleMaps)sample).good = new OrderRetainingMap();
         ((SampleMaps)sample).good.put("Windows", new Software("Microsoft", "Windows"));
         ((SampleMaps)sample).good.put("Linux", new Software("Red Hat", "Linux"));
         sample.good.put("Android", new Software("Google", "Android"));
@@ -503,12 +504,12 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
         String expected = "" +
                 "<sample2>\n" +
                 "  <software defined-in=\"sample\">\n" +
-                "    <vendor>Red Hat</vendor>\n" +
-                "    <name>Linux</name>\n" +
-                "  </software>\n" +
-                "  <software defined-in=\"sample\">\n" +
                 "    <vendor>Microsoft</vendor>\n" +
                 "    <name>Windows</name>\n" +
+                "  </software>\n" +
+                "  <software defined-in=\"sample\">\n" +
+                "    <vendor>Red Hat</vendor>\n" +
+                "    <name>Linux</name>\n" +
                 "  </software>\n" +
                 "  <software>\n" +
                 "    <vendor>Google</vendor>\n" +
@@ -527,6 +528,7 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
     
     public void testWithHiddenMapAndDifferentAlias() {
         SampleMaps2 sample = new SampleMaps2();
+        ((SampleMaps)sample).good = new OrderRetainingMap();
         ((SampleMaps)sample).good.put("Windows", new Software("Microsoft", "Windows"));
         ((SampleMaps)sample).good.put("Linux", new Software("Red Hat", "Linux"));
         sample.good.put("Android", new Software("Google", "Android"));
@@ -536,12 +538,12 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
         String expected = "" +
                 "<sample2>\n" +
                 "  <code defined-in=\"sample\">\n" +
-                "    <vendor>Red Hat</vendor>\n" +
-                "    <name>Linux</name>\n" +
-                "  </code>\n" +
-                "  <code defined-in=\"sample\">\n" +
                 "    <vendor>Microsoft</vendor>\n" +
                 "    <name>Windows</name>\n" +
+                "  </code>\n" +
+                "  <code defined-in=\"sample\">\n" +
+                "    <vendor>Red Hat</vendor>\n" +
+                "    <name>Linux</name>\n" +
                 "  </code>\n" +
                 "  <mobile>\n" +
                 "    <vendor>Google</vendor>\n" +
@@ -559,6 +561,7 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
     
     public void testDoesNotInheritFromHiddenMapOfSuperclass() {
         SampleMaps2 sample = new SampleMaps2();
+        ((SampleMaps)sample).good = new OrderRetainingMap();
         ((SampleMaps)sample).good.put("Windows", new Software("Microsoft", "Windows"));
         ((SampleMaps)sample).good.put("Linux", new Software("Red Hat", "Linux"));
         sample.good.put("Android", new Software("Google", "Android"));
@@ -568,12 +571,12 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
         String expected = "" +
                 "<sample2>\n" +
                 "  <software defined-in=\"sample\">\n" +
-                "    <vendor>Red Hat</vendor>\n" +
-                "    <name>Linux</name>\n" +
-                "  </software>\n" +
-                "  <software defined-in=\"sample\">\n" +
                 "    <vendor>Microsoft</vendor>\n" +
                 "    <name>Windows</name>\n" +
+                "  </software>\n" +
+                "  <software defined-in=\"sample\">\n" +
+                "    <vendor>Red Hat</vendor>\n" +
+                "    <name>Linux</name>\n" +
                 "  </software>\n" +
                 "  <good>\n" +
                 "    <entry>\n" +
@@ -599,6 +602,7 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
     
     public void testDoesNotPropagateToHiddenMapOfSuperclass() {
         SampleMaps2 sample = new SampleMaps2();
+        ((SampleMaps)sample).good = new OrderRetainingMap();
         ((SampleMaps)sample).good.put("Windows", new Software("Microsoft", "Windows"));
         ((SampleMaps)sample).good.put("Linux", new Software("Red Hat", "Linux"));
         sample.good.put("Android", new Software("Google", "Android"));
@@ -609,17 +613,17 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
                 "<sample2>\n" +
                 "  <good defined-in=\"sample\">\n" +
                 "    <entry>\n" +
-                "      <string>Linux</string>\n" +
-                "      <software>\n" +
-                "        <vendor>Red Hat</vendor>\n" +
-                "        <name>Linux</name>\n" +
-                "      </software>\n" +
-                "    </entry>\n" +
-                "    <entry>\n" +
                 "      <string>Windows</string>\n" +
                 "      <software>\n" +
                 "        <vendor>Microsoft</vendor>\n" +
                 "        <name>Windows</name>\n" +
+                "      </software>\n" +
+                "    </entry>\n" +
+                "    <entry>\n" +
+                "      <string>Linux</string>\n" +
+                "      <software>\n" +
+                "        <vendor>Red Hat</vendor>\n" +
+                "        <name>Linux</name>\n" +
                 "      </software>\n" +
                 "    </entry>\n" +
                 "  </good>\n" +
@@ -646,6 +650,7 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
     
     public void testWithDoubleHiddenList() {
         SampleMaps3 sample = new SampleMaps3();
+        ((SampleMaps)sample).good = new OrderRetainingMap();
         ((SampleMaps)sample).good.put("Windows", new Software("Microsoft", "Windows"));
         ((SampleMaps)sample).good.put("Linux", new Software("Red Hat", "Linux"));
         ((SampleMaps2)sample).good.put("Android", new Software("Google", "Android"));
@@ -657,12 +662,12 @@ public class ImplicitMapTest extends AbstractAcceptanceTest {
         String expected = "" +
                 "<sample3>\n" +
                 "  <software defined-in=\"sample\">\n" +
-                "    <vendor>Red Hat</vendor>\n" +
-                "    <name>Linux</name>\n" +
-                "  </software>\n" +
-                "  <software defined-in=\"sample\">\n" +
                 "    <vendor>Microsoft</vendor>\n" +
                 "    <name>Windows</name>\n" +
+                "  </software>\n" +
+                "  <software defined-in=\"sample\">\n" +
+                "    <vendor>Red Hat</vendor>\n" +
+                "    <name>Linux</name>\n" +
                 "  </software>\n" +
                 "  <software defined-in=\"sample2\">\n" +
                 "    <vendor>Google</vendor>\n" +
