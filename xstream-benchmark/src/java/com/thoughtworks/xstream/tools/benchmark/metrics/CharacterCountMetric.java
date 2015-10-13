cdc44b40ab45fbb3405ebf37ffef06bc4765c110
joehni <joerg.schaible@gmx.de>
2015-10-14 00:16:52 +0200

    Deprecate benchmark module.

--- xstream-benchmark/src/java/com/thoughtworks/xstream/tools/benchmark/metrics/CharacterCountMetric.java
+++ xstream-benchmark/src/java/com/thoughtworks/xstream/tools/benchmark/metrics/CharacterCountMetric.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008, 2009, 2011 XStream Committers.
+ * Copyright (C) 2007, 2008, 2009, 2011, 2015 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -21,6 +21,7 @@ import java.io.ByteArrayOutputStream;
  *
  * @author J&ouml;rg Schaible
  * @since 1.4
+ * @deprecated As of upcoming use JMH instead
  */
 public class CharacterCountMetric implements Metric {
     
