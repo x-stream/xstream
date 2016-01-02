0adb08c9ac2395c39940cda0037d64a7b774a3ee
joehni <joerg.schaible@gmx.de>
2016-01-02 20:35:23 +0100

    Fix: Possible concurrency problem with XomDriver.

--- xstream/src/java/com/thoughtworks/xstream/io/xml/XomDriver.java
+++ xstream/src/java/com/thoughtworks/xstream/io/xml/XomDriver.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2006 Joe Walnes.
- * Copyright (C) 2006, 2007, 2009, 2011, 2014 XStream Committers.
+ * Copyright (C) 2006, 2007, 2009, 2011, 2014, 2016 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
- * 
+ *
  * Created on 12. April 2006 by Joerg Schaible
  */
 package com.thoughtworks.xstream.io.xml;
@@ -36,9 +36,13 @@ public class XomDriver extends AbstractXmlDriver {
     private final Builder builder;
 
     public XomDriver() {
-        this(new Builder());
+        this(new XmlFriendlyNameCoder());
     }
 
+    /**
+     * @deprecated As of 1.4.9, use {@link #XomDriver()} and overload {@link #createBuilder()} instead
+     */
+    @Deprecated
     public XomDriver(final Builder builder) {
         this(builder, new XmlFriendlyNameCoder());
     }
@@ -47,12 +51,15 @@ public class XomDriver extends AbstractXmlDriver {
      * @since 1.4
      */
     public XomDriver(final NameCoder nameCoder) {
-        this(new Builder(), nameCoder);
+        super(nameCoder);
+        builder = null;
     }
 
     /**
      * @since 1.4
+     * @deprecated As of 1.4.9, use {@link #XomDriver(NameCoder)} and overload {@link #createBuilder()} instead
      */
+    @Deprecated
     public XomDriver(final Builder builder, final NameCoder nameCoder) {
         super(nameCoder);
         this.builder = builder;
@@ -60,30 +67,47 @@ public class XomDriver extends AbstractXmlDriver {
 
     /**
      * @since 1.2
-     * @deprecated As of 1.4, use {@link #XomDriver(Builder, NameCoder)} instead
+     * @deprecated As of 1.4, use {@link #XomDriver(NameCoder)} instead
      */
     @Deprecated
     public XomDriver(final XmlFriendlyReplacer replacer) {
-        this(new Builder(), replacer);
+        this((NameCoder)replacer);
     }
 
     /**
      * @since 1.2
-     * @deprecated As of 1.4, use {@link #XomDriver(Builder, NameCoder)} instead
+     * @deprecated As of 1.4, use {@link #XomDriver(NameCoder)} and overload {@link #createBuilder()} instead
      */
     @Deprecated
     public XomDriver(final Builder builder, final XmlFriendlyReplacer replacer) {
-        this((NameCoder)replacer);
+        this(builder, (NameCoder)replacer);
     }
 
+    /**
+     * @deprecated As of 1.4.9, overload {@link #createBuilder()} instead
+     */
+    @Deprecated
     protected Builder getBuilder() {
         return builder;
+    }
+
+    /**
+     * Create the Builder instance. A XOM builder is a wrapper around a {@link org.xml.sax.XMLReader} instance which is
+     * not thread-safe by definition. Therefore each reader should use its own builder instance to avoid concurrency
+     * problems. Overload this method to configure the generated builder instances e.g. to activate validation.
+     *
+     * @return the new builder
+     * @since 1.4.9
+     */
+    protected Builder createBuilder() {
+        final Builder builder = getBuilder();
+        return builder != null ? builder : new Builder();
     }
 
     @Override
     public HierarchicalStreamReader createReader(final Reader text) {
         try {
-            final Document document = getBuilder().build(text);
+            final Document document = createBuilder().build(text);
             return new XomReader(document, getNameCoder());
         } catch (final ValidityException e) {
             throw new StreamException(e);
@@ -97,7 +121,7 @@ public class XomDriver extends AbstractXmlDriver {
     @Override
     public HierarchicalStreamReader createReader(final InputStream in) {
         try {
-            final Document document = getBuilder().build(in);
+            final Document document = createBuilder().build(in);
             return new XomReader(document, getNameCoder());
         } catch (final ValidityException e) {
             throw new StreamException(e);
@@ -111,7 +135,7 @@ public class XomDriver extends AbstractXmlDriver {
     @Override
     public HierarchicalStreamReader createReader(final URL in) {
         try {
-            final Document document = getBuilder().build(in.toExternalForm());
+            final Document document = createBuilder().build(in.toExternalForm());
             return new XomReader(document, getNameCoder());
         } catch (final ValidityException e) {
             throw new StreamException(e);
@@ -125,7 +149,7 @@ public class XomDriver extends AbstractXmlDriver {
     @Override
     public HierarchicalStreamReader createReader(final File in) {
         try {
-            final Document document = getBuilder().build(in);
+            final Document document = createBuilder().build(in);
             return new XomReader(document, getNameCoder());
         } catch (final ValidityException e) {
             throw new StreamException(e);
