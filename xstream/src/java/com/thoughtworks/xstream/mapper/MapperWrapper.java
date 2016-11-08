79c6cb8d6c885f8c086ffd5adae2d21718a996e7
joehni <joerg.schaible@gmx.de>
2016-11-08 20:16:17 +0100

    Improve performance by minimizing call stack of mapper chain.

--- xstream/src/java/com/thoughtworks/xstream/mapper/MapperWrapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/MapperWrapper.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2005, 2006 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2009, 2015 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2015, 2016 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -11,129 +11,222 @@
  */
 package com.thoughtworks.xstream.mapper;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.SingleValueConverter;
 
-public abstract class MapperWrapper implements Mapper {
 
+public abstract class MapperWrapper implements Mapper {
     private final Mapper wrapped;
+    private final Mapper aliasForAttributeMapper;
+    private final Mapper aliasForSystemAttributeMapper;
+    private final Mapper attributeForAliasMapper;
+    private final Mapper defaultImplementationOfMapper;
+    private final Mapper getConverterFromAttributeMapper;
+    private final Mapper getConverterFromItemTypeMapper;
+    private final Mapper getFieldNameForItemTypeAndNameMapper;
+    private final Mapper getImplicitCollectionDefForFieldNameMapper;
+    private final Mapper getItemTypeForItemFieldNameMapper;
+    private final Mapper getLocalConverterMapper;
+    private final Mapper isImmutableValueTypeMapper;
+    private final Mapper isReferenceableMapper;
+    private final Mapper realClassMapper;
+    private final Mapper realMemberMapper;
+    private final Mapper serializedClassMapper;
+    private final Mapper serializedMemberMapper;
+    private final Mapper shouldSerializeMemberMapper;
 
     public MapperWrapper(Mapper wrapped) {
         this.wrapped = wrapped;
+
+        if (wrapped instanceof MapperWrapper) {
+            final MapperWrapper wrapper = (MapperWrapper)wrapped;
+            final Map wrapperMap = new HashMap();
+            wrapperMap.put("aliasForAttribute", wrapper.aliasForAttributeMapper);
+            wrapperMap.put("aliasForSystemAttribute", wrapper.aliasForSystemAttributeMapper);
+            wrapperMap.put("attributeForAlias", wrapper.attributeForAliasMapper);
+            wrapperMap.put("defaultImplementationOf", wrapper.defaultImplementationOfMapper);
+            wrapperMap.put("getConverterFromAttribute", wrapper.getConverterFromAttributeMapper);
+            wrapperMap.put("getConverterFromItemType", wrapper.getConverterFromItemTypeMapper);
+            wrapperMap.put("getFieldNameForItemTypeAndName", wrapper.getFieldNameForItemTypeAndNameMapper);
+            wrapperMap.put("getImplicitCollectionDefForFieldName", wrapper.getImplicitCollectionDefForFieldNameMapper);
+            wrapperMap.put("getItemTypeForItemFieldName", wrapper.getItemTypeForItemFieldNameMapper);
+            wrapperMap.put("getLocalConverter", wrapper.getLocalConverterMapper);
+            wrapperMap.put("isImmutableValueType", wrapper.isImmutableValueTypeMapper);
+            wrapperMap.put("isReferenceable", wrapper.isReferenceableMapper);
+            wrapperMap.put("realClass", wrapper.realClassMapper);
+            wrapperMap.put("realMember", wrapper.realMemberMapper);
+            wrapperMap.put("serializedClass", wrapper.serializedClassMapper);
+            wrapperMap.put("serializedMember", wrapper.serializedMemberMapper);
+            wrapperMap.put("shouldSerializeMember", wrapper.shouldSerializeMemberMapper);
+
+            final Method[] methods = wrapped.getClass().getMethods();
+            for (int i = 0; i < methods.length; ++i) {
+                final Method method = methods[i];
+                if (method.getDeclaringClass() != MapperWrapper.class) {
+                    final String name = method.getName();
+                    if (wrapperMap.containsKey(name)) {
+                        wrapperMap.put(name, wrapped);
+                    }
+                }
+            }
+
+            aliasForAttributeMapper = (Mapper)wrapperMap.get("aliasForAttribute");
+            aliasForSystemAttributeMapper = (Mapper)wrapperMap.get("aliasForSystemAttribute");
+            attributeForAliasMapper = (Mapper)wrapperMap.get("attributeForAlias");
+            defaultImplementationOfMapper = (Mapper)wrapperMap.get("defaultImplementationOf");
+            getConverterFromAttributeMapper = (Mapper)wrapperMap.get("getConverterFromAttribute");
+            getConverterFromItemTypeMapper = (Mapper)wrapperMap.get("getConverterFromItemType");
+            getFieldNameForItemTypeAndNameMapper = (Mapper)wrapperMap.get("getFieldNameForItemTypeAndName");
+            getImplicitCollectionDefForFieldNameMapper = (Mapper)wrapperMap.get("getImplicitCollectionDefForFieldName");
+            getItemTypeForItemFieldNameMapper = (Mapper)wrapperMap.get("getItemTypeForItemFieldName");
+            getLocalConverterMapper = (Mapper)wrapperMap.get("getLocalConverter");
+            isImmutableValueTypeMapper = (Mapper)wrapperMap.get("isImmutableValueType");
+            isReferenceableMapper = (Mapper)wrapperMap.get("isReferenceable");
+            realClassMapper = (Mapper)wrapperMap.get("realClass");
+            realMemberMapper = (Mapper)wrapperMap.get("realMember");
+            serializedClassMapper = (Mapper)wrapperMap.get("serializedClass");
+            serializedMemberMapper = (Mapper)wrapperMap.get("serializedMember");
+            shouldSerializeMemberMapper = (Mapper)wrapperMap.get("shouldSerializeMember");
+        } else {
+            aliasForAttributeMapper = wrapped;
+            aliasForSystemAttributeMapper = wrapped;
+            attributeForAliasMapper = wrapped;
+            defaultImplementationOfMapper = wrapped;
+            getConverterFromAttributeMapper = wrapped;
+            getConverterFromItemTypeMapper = wrapped;
+            getFieldNameForItemTypeAndNameMapper = wrapped;
+            getImplicitCollectionDefForFieldNameMapper = wrapped;
+            getItemTypeForItemFieldNameMapper = wrapped;
+            getLocalConverterMapper = wrapped;
+            isImmutableValueTypeMapper = wrapped;
+            isReferenceableMapper = wrapped;
+            realClassMapper = wrapped;
+            realMemberMapper = wrapped;
+            serializedClassMapper = wrapped;
+            serializedMemberMapper = wrapped;
+            shouldSerializeMemberMapper = wrapped;
+        }
+
     }
 
     public String serializedClass(Class type) {
-        return wrapped.serializedClass(type);
+        return serializedClassMapper.serializedClass(type);
     }
 
     public Class realClass(String elementName) {
-        return wrapped.realClass(elementName);
+        return realClassMapper.realClass(elementName);
     }
 
     public String serializedMember(Class type, String memberName) {
-        return wrapped.serializedMember(type, memberName);
+        return serializedMemberMapper.serializedMember(type, memberName);
     }
 
     public String realMember(Class type, String serialized) {
-        return wrapped.realMember(type, serialized);
+        return realMemberMapper.realMember(type, serialized);
     }
 
     public boolean isImmutableValueType(Class type) {
-        return wrapped.isImmutableValueType(type);
+        return isImmutableValueTypeMapper.isImmutableValueType(type);
     }
 
     public boolean isReferenceable(Class type) {
-        return wrapped.isReferenceable(type);
+        return isReferenceableMapper.isReferenceable(type);
     }
 
     public Class defaultImplementationOf(Class type) {
-        return wrapped.defaultImplementationOf(type);
+        return defaultImplementationOfMapper.defaultImplementationOf(type);
     }
 
     public String aliasForAttribute(String attribute) {
-        return wrapped.aliasForAttribute(attribute);
+        return aliasForAttributeMapper.aliasForAttribute(attribute);
     }
 
     public String attributeForAlias(String alias) {
-        return wrapped.attributeForAlias(alias);
+        return attributeForAliasMapper.attributeForAlias(alias);
     }
 
     public String aliasForSystemAttribute(String attribute) {
-        return wrapped.aliasForSystemAttribute(attribute);
+        return aliasForSystemAttributeMapper.aliasForSystemAttribute(attribute);
     }
 
     public String getFieldNameForItemTypeAndName(Class definedIn, Class itemType, String itemFieldName) {
-        return wrapped.getFieldNameForItemTypeAndName(definedIn, itemType, itemFieldName);
+        return getFieldNameForItemTypeAndNameMapper.getFieldNameForItemTypeAndName(definedIn, itemType, itemFieldName);
     }
 
     public Class getItemTypeForItemFieldName(Class definedIn, String itemFieldName) {
-        return wrapped.getItemTypeForItemFieldName(definedIn, itemFieldName);
+        return getItemTypeForItemFieldNameMapper.getItemTypeForItemFieldName(definedIn, itemFieldName);
     }
 
     public ImplicitCollectionMapping getImplicitCollectionDefForFieldName(Class itemType, String fieldName) {
-        return wrapped.getImplicitCollectionDefForFieldName(itemType, fieldName);
+        return getImplicitCollectionDefForFieldNameMapper.getImplicitCollectionDefForFieldName(itemType, fieldName);
     }
 
     public boolean shouldSerializeMember(Class definedIn, String fieldName) {
-        return wrapped.shouldSerializeMember(definedIn, fieldName);
+        return shouldSerializeMemberMapper.shouldSerializeMember(definedIn, fieldName);
     }
 
     /**
      * @deprecated As of 1.3, use {@link #getConverterFromItemType(String, Class, Class)}
      */
     public SingleValueConverter getConverterFromItemType(String fieldName, Class type) {
-        return wrapped.getConverterFromItemType(fieldName, type);
+        return getConverterFromItemTypeMapper.getConverterFromItemType(fieldName, type);
     }
 
     /**
      * @deprecated As of 1.3, use {@link #getConverterFromItemType(String, Class, Class)}
      */
     public SingleValueConverter getConverterFromItemType(Class type) {
-        return wrapped.getConverterFromItemType(type);
+        return getConverterFromItemTypeMapper.getConverterFromItemType(type);
     }
 
     /**
      * @deprecated As of 1.3, use {@link #getConverterFromAttribute(Class, String, Class)}
      */
     public SingleValueConverter getConverterFromAttribute(String name) {
-        return wrapped.getConverterFromAttribute(name);
+        return getConverterFromAttributeMapper.getConverterFromAttribute(name);
     }
 
     public Converter getLocalConverter(Class definedIn, String fieldName) {
-        return wrapped.getLocalConverter(definedIn, fieldName);
+        return getLocalConverterMapper.getLocalConverter(definedIn, fieldName);
     }
 
     public Mapper lookupMapperOfType(Class type) {
         return type.isAssignableFrom(getClass()) ? this : wrapped.lookupMapperOfType(type);
     }
-    
+
     public SingleValueConverter getConverterFromItemType(String fieldName, Class type, Class definedIn) {
-    	return wrapped.getConverterFromItemType(fieldName, type, definedIn);
+        return getConverterFromItemTypeMapper.getConverterFromItemType(fieldName, type, definedIn);
     }
-    
+
     /**
-     * @deprecated As of 1.3, use combination of {@link #serializedMember(Class, String)} and {@link #getConverterFromItemType(String, Class, Class)} 
+     * @deprecated As of 1.3, use combination of {@link #serializedMember(Class, String)} and
+     *             {@link #getConverterFromItemType(String, Class, Class)}
      */
     public String aliasForAttribute(Class definedIn, String fieldName) {
-    	return wrapped.aliasForAttribute(definedIn, fieldName);
+        return aliasForAttributeMapper.aliasForAttribute(definedIn, fieldName);
     }
-    
+
     /**
-     * @deprecated As of 1.3, use combination of {@link #realMember(Class, String)} and {@link #getConverterFromItemType(String, Class, Class)} 
+     * @deprecated As of 1.3, use combination of {@link #realMember(Class, String)} and
+     *             {@link #getConverterFromItemType(String, Class, Class)}
      */
     public String attributeForAlias(Class definedIn, String alias) {
-    	return wrapped.attributeForAlias(definedIn, alias);
+        return attributeForAliasMapper.attributeForAlias(definedIn, alias);
     }
-    
+
     /**
-     * @deprecated As of 1.3.1, use {@link #getConverterFromAttribute(Class, String, Class)} 
+     * @deprecated As of 1.3.1, use {@link #getConverterFromAttribute(Class, String, Class)}
      */
     public SingleValueConverter getConverterFromAttribute(Class type, String attribute) {
-    	return wrapped.getConverterFromAttribute(type, attribute);
+        return getConverterFromAttributeMapper.getConverterFromAttribute(type, attribute);
     }
-    
+
     public SingleValueConverter getConverterFromAttribute(Class definedIn, String attribute, Class type) {
-        return wrapped.getConverterFromAttribute(definedIn, attribute, type);
+        return getConverterFromAttributeMapper.getConverterFromAttribute(definedIn, attribute, type);
     }
 
 }
