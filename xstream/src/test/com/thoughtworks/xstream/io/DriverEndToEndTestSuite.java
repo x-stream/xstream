38d5ae59ad0cb652f25557570ecc6f507342c4a0
joehni <joerg.schaible@gmx.de>
2016-12-23 17:20:21 +0100

    Add second test for stream behavior.

--- xstream/src/test/com/thoughtworks/xstream/io/DriverEndToEndTestSuite.java
+++ xstream/src/test/com/thoughtworks/xstream/io/DriverEndToEndTestSuite.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2005 Joe Walnes.
- * Copyright (C) 2006, 2007, 2011, 2013, 2014 XStream Committers.
+ * Copyright (C) 2006, 2007, 2011, 2013, 2014, 2016 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
- * 
+ *
  * Created on 30. April 2005 by Joe Walnes
  */
 package com.thoughtworks.xstream.io;
@@ -25,6 +25,7 @@ import com.thoughtworks.xstream.io.binary.BinaryStreamDriver;
 import com.thoughtworks.xstream.io.xml.BEAStaxDriver;
 import com.thoughtworks.xstream.io.xml.Dom4JDriver;
 import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.io.xml.JDom2Driver;
 import com.thoughtworks.xstream.io.xml.JDomDriver;
 import com.thoughtworks.xstream.io.xml.KXml2DomDriver;
 import com.thoughtworks.xstream.io.xml.KXml2Driver;
@@ -42,6 +43,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+
 public class DriverEndToEndTestSuite extends TestSuite {
 
     public static Test suite() {
@@ -56,12 +58,12 @@ public class DriverEndToEndTestSuite extends TestSuite {
         addDriverTest(new DomDriver());
         addDriverTest(new JDomDriver());
         if (JVM.is15()) {
-            Class driverType = JVM.loadClassForName("com.thoughtworks.xstream.io.xml.JDom2Driver");
+            final Class driverType = JVM.loadClassForName("com.thoughtworks.xstream.io.xml.JDom2Driver");
             try {
                 addDriverTest((HierarchicalStreamDriver)driverType.newInstance());
-            } catch (InstantiationException e) {
+            } catch (final InstantiationException e) {
                 throw new AssertionFailedError("Cannot instantiate " + driverType.getName());
-            } catch (IllegalAccessException e) {
+            } catch (final IllegalAccessException e) {
                 throw new AssertionFailedError("Cannot access default constructor of " + driverType.getName());
             }
         }
@@ -69,12 +71,12 @@ public class DriverEndToEndTestSuite extends TestSuite {
         addDriverTest(new KXml2Driver());
         addDriverTest(new StaxDriver());
         if (JVM.is16()) {
-            Class driverType = JVM.loadClassForName("com.thoughtworks.xstream.io.xml.StandardStaxDriver");
+            final Class driverType = JVM.loadClassForName("com.thoughtworks.xstream.io.xml.StandardStaxDriver");
             try {
                 addDriverTest((HierarchicalStreamDriver)driverType.newInstance());
-            } catch (InstantiationException e) {
+            } catch (final InstantiationException e) {
                 throw new AssertionFailedError("Cannot instantiate " + driverType.getName());
-            } catch (IllegalAccessException e) {
+            } catch (final IllegalAccessException e) {
                 throw new AssertionFailedError("Cannot access default constructor of " + driverType.getName());
             }
         }
@@ -85,57 +87,101 @@ public class DriverEndToEndTestSuite extends TestSuite {
         addDriverTest(new XppDomDriver());
         addDriverTest(new XppDriver());
         if (JVM.is14()) {
-            Class driverType = JVM.loadClassForName("com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver");
+            final Class driverType = JVM.loadClassForName("com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver");
             try {
                 addDriverTest((HierarchicalStreamDriver)driverType.newInstance());
-            } catch (InstantiationException e) {
+            } catch (final InstantiationException e) {
                 throw new AssertionFailedError("Cannot instantiate " + driverType.getName());
-            } catch (IllegalAccessException e) {
+            } catch (final IllegalAccessException e) {
                 throw new AssertionFailedError("Cannot access default constructor of " + driverType.getName());
             }
         }
     }
 
-    private void test(HierarchicalStreamDriver driver) {
-        XStream xstream = new XStream(driver);
-        xstream.allowTypesByWildcard(AbstractAcceptanceTest.class.getPackage().getName()+".*objects.**");
-        xstream.allowTypesByWildcard(this.getClass().getName()+"$*");
+    private void testObject(final HierarchicalStreamDriver driver) {
+        final XStream xstream = new XStream(driver);
+        xstream.allowTypesByWildcard(AbstractAcceptanceTest.class.getPackage().getName() + ".*objects.**");
+        xstream.allowTypesByWildcard(this.getClass().getName() + "$*");
         xstream.registerConverter(new CollectionConverter(xstream.getMapper()) {
 
-            public Object unmarshal(HierarchicalStreamReader reader,
-                UnmarshallingContext context) {
-                ExtendedHierarchicalStreamReader exReader = (ExtendedHierarchicalStreamReader)reader;
+            @Override
+            public Object unmarshal(final HierarchicalStreamReader reader, final UnmarshallingContext context) {
+                final ExtendedHierarchicalStreamReader exReader = (ExtendedHierarchicalStreamReader)reader;
                 if (exReader.peekNextChild() == null) {
                     return new ArrayList();
                 }
                 return super.unmarshal(reader, context);
             }
-            
+
         });
 
-        SampleLists in = new SampleLists();
+        final SampleLists in = new SampleLists();
         in.good.add("one");
         in.good.add("two");
         in.good.add("three");
         in.bad.add(Boolean.TRUE);
         in.bad.add(Boolean.FALSE);
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         xstream.toXML(in, buffer);
-        Object out = xstream.fromXML(new ByteArrayInputStream(buffer.toByteArray()));
+        final Object out = xstream.fromXML(new ByteArrayInputStream(buffer.toByteArray()));
 
         Assert.assertEquals(in, out);
     }
 
+    private void testStream(final HierarchicalStreamDriver driver) {
+        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        final HierarchicalStreamWriter writer = driver.createWriter(buffer);
+        writer.startNode("root");
+        writer.startNode("child1");
+        writer.startNode("baby");
+        writer.endNode();
+        writer.endNode();
+        writer.startNode("child2");
+        writer.addAttribute("A", "a");
+        writer.setValue("v");
+        writer.endNode();
+        writer.endNode();
+        writer.close();
+
+        final HierarchicalStreamReader reader = driver.createReader(new ByteArrayInputStream(buffer.toByteArray()));
+        Assert.assertEquals("root", reader.getNodeName());
+        Assert.assertTrue(reader.hasMoreChildren());
+        reader.moveDown();
+        Assert.assertEquals("child1", reader.getNodeName());
+        Assert.assertEquals(0, reader.getAttributeCount());
+        Assert.assertNull(reader.getAttribute("foo"));
+        Assert.assertTrue(reader.hasMoreChildren());
+        reader.moveUp();
+        Assert.assertTrue(reader.hasMoreChildren());
+        reader.moveDown();
+        Assert.assertEquals("child2", reader.getNodeName());
+        Assert.assertEquals(1, reader.getAttributeCount());
+        Assert.assertEquals("a", reader.getAttribute("A"));
+        Assert.assertNull(reader.getAttribute("foo"));
+        //Assert.assertNull(reader.getAttribute(1));
+        Assert.assertFalse(reader.hasMoreChildren());
+        reader.moveUp();
+        Assert.assertFalse(reader.hasMoreChildren());
+        reader.close();
+    }
+
     private void addDriverTest(final HierarchicalStreamDriver driver) {
-        String testName = getShortName(driver);
-        addTest(new TestCase(testName) {
+        final String testName = getShortName(driver);
+        addTest(new TestCase(testName + "_Object") {
+            @Override
             protected void runTest() throws Throwable {
-                test(driver);
+                testObject(driver);
+            }
+        });
+        addTest(new TestCase(testName + "_Stream") {
+            @Override
+            protected void runTest() throws Throwable {
+                testStream(driver);
             }
         });
     }
 
-    private String getShortName(HierarchicalStreamDriver driver) {
+    private String getShortName(final HierarchicalStreamDriver driver) {
         String result = driver.getClass().getName();
         result = result.substring(result.lastIndexOf('.') + 1);
         return result;
