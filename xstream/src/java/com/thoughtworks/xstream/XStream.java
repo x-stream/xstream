b81fd21afd66acfc418667bcf9d01f8ffb9f5271
joehni <joerg.schaible@gmx.de>
2017-02-18 15:19:41 +0100

    Add aliases for clock implementations.

--- xstream/src/java/com/thoughtworks/xstream/XStream.java
+++ xstream/src/java/com/thoughtworks/xstream/XStream.java
@@ -660,6 +660,7 @@ public class XStream {
             types.add(JVM.loadClassForName("java.sql.Date"));
         }
         if (JVM.is18()) {
+            allowTypeHierarchy(JVM.loadClassForName("java.time.Clock"));
             types.add(JVM.loadClassForName("java.time.Duration"));
             types.add(JVM.loadClassForName("java.time.Instant"));
             types.add(JVM.loadClassForName("java.time.LocalDate"));
@@ -758,6 +759,10 @@ public class XStream {
         }
 
         if (JVM.is18()) {
+            alias("fixed-clock", JVM.loadClassForName("java.time.Clock$FixedClock"));
+            alias("offset-clock", JVM.loadClassForName("java.time.Clock$OffsetClock"));
+            alias("system-clock", JVM.loadClassForName("java.time.Clock$SystemClock"));
+            alias("tick-clock", JVM.loadClassForName("java.time.Clock$TickClock"));
             alias("duration", JVM.loadClassForName("java.time.Duration"));
             alias("instant", JVM.loadClassForName("java.time.Instant"));
             alias("local-date", JVM.loadClassForName("java.time.LocalDate"));
@@ -893,6 +898,7 @@ public class XStream {
         registerConverter(new JavaClassConverter(classLoaderReference), PRIORITY_NORMAL);
         registerConverter(new JavaMethodConverter(classLoaderReference), PRIORITY_NORMAL);
         registerConverter(new JavaFieldConverter(classLoaderReference), PRIORITY_NORMAL);
+
         if (JVM.isAWTAvailable()) {
             registerConverter(new FontConverter(mapper), PRIORITY_NORMAL);
             registerConverter(new ColorConverter(), PRIORITY_NORMAL);
--- xstream/src/java/com/thoughtworks/xstream/core/util/SerializationMembers.java
+++ xstream/src/java/com/thoughtworks/xstream/core/util/SerializationMembers.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2004, 2005 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2010, 2011, 2014, 2015, 2016 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2010, 2011, 2014, 2015, 2016, 2017 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
