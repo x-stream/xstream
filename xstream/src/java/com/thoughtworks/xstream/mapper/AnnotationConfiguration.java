e94be3f16654bb37900b677826df18b1fa0626ad
joehni <joerg.schaible@gmx.de>
2018-01-25 23:53:07 +0100

    Fix version info in javadoc.

--- xstream/src/java/com/thoughtworks/xstream/mapper/AnnotationConfiguration.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/AnnotationConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008, 2013 XStream Committers.
+ * Copyright (C) 2007, 2008, 2013, 2018 XStream Committers.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -15,7 +15,7 @@ package com.thoughtworks.xstream.mapper;
  * 
  * @author J&ouml;rg Schaible
  * @since 1.3
- * @deprecated As of 1.4.5, minimal JDK version will be 1.6 for next major release
+ * @deprecated As of 1.4.5, minimal JDK version will be 1.7 for next major release
  */
 public interface AnnotationConfiguration {
 
