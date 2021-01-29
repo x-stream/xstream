1c606f8709ee652557988d53d5fdc53877fc5b2c
Julia Boes <julia.boes@oracle.com>
2021-01-29 12:44:42 +0000

    fix copyright year

--- xstream/src/java/com/thoughtworks/xstream/XStream.java
+++ xstream/src/java/com/thoughtworks/xstream/XStream.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2003, 2004, 2005, 2006 Joe Walnes.
- * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021 XStream Committers.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 XStream Committers.
  * Copyright (C) 2020, Oracle and/or its affiliates.
  * All rights reserved.
  *
